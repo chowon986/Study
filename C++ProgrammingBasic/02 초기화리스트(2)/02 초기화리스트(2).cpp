0ced608f75233094dc5b7232e1584e8504076bb0
chowon986 <chowon986@gmail.com>
2022-03-15 19:20:00 +0900

    Section 02 : 3강 내용 정리

---
+++ C++ProgrammingBasic/02 초기화리스트(2)/02 초기화리스트(2).cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+
+class Vector
+{
+public:
+    Vector(int size) 
+    {
+
+    }
+};
+
+void foo(Vector V)
+{
+
+}
+
+class Object
+{
+};
+
+void foo(Object obj)
+{
+
+}
+
+int main()
+{
+    Vector v1(10);      // 직접 초기화
+    Vector v2 = 10;     // 복사 초기화
+    Vector v3{ 10 };    // 직접 초기화
+    Vector v4 = { 10 }; // 복사 초기화
+
+    // 인자가 1개인 생성자는 변환의 용도로 사용될 수 있다.
+    v1 = 20; // v1 = Vector(20); // 변환 생성자
+    foo(v1); // ok
+    foo(10); // foo()가 Vector를 받는데 10을 넣어도 에러가 나지 않는다.
+             // Vector v = 10; 복사 초기화가 된다. -> 일부러 하기 보다는 보통 다른 함수에 넣을 값을 실수로 한다.
+             // 아래 방법으로 예방 가능
+
+
+    Object o1;
+    Object o2{};
+    Object o3 = {};
+}
+/* Vector class 안에 아래와 같이 넣으면
+explicit Vector(int size) // explicit는 직접 초기화는 가능한데 복사 초기화는 불가하다.
+                          // 암시적 변환의 용도로 사용될 수 없다.
+{
+
+}
+*/
+
+// STL과 explicit 생성자
+// string = explicit 아님
+// vector = explicit 임
+
+// vector는 v3 = 10; 은 에러지만 v4 = {10}은 에러나지 않는다.
+// v4는 list (explicit 아님) 때문에
+
+// explicit는 인자의 개수가 없거나 여러 개인 경우에도 붙일 수 있다.
+
+
+// explicit(true) 라고하면 explicit다
+// explicit(false) 라고하면 explicit다.
+// false라고 적을거면 두번째를 안해도 되지 않나요?
+// 주로 템플릿에서 씁니다.
+template<typename T>
+explicit(std::is_integral_v<T>) Object(T arg) {}
+// 정수형 배열이면 true, 그렇지 않으면 false로 해라 이런 식으루
---
+++ C++ProgrammingBasic/02 초기화리스트/02 초기화리스트.cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+#include <string>
+
+// 초기화 리스트 #1
+
+class Chowon
+{
+	std::string name/*{s}*/; // 여기서 바로 초기화도 가능
+	int age/*{a}*/; 
+public:
+	Chowon(const std::string& s, int a)
+		/*(1)*/ : name{s}, age{a} // 초기화 -> 객체를 생성하면서 초기값을 전달하는 것
+								  // 즉, 한번의 함수 호출로 초기화
+								  // (2) : name(s), age(a)도 가능
+	{
+		//(3) name = s; sge = a;  // 대입
+								  // 즉, 2번의 함수 호출 발생
+	}
+};
+// 1, 2번 방법이 3번에 비해 더 효율적이다.
+
+// 멤버 초기화 리스트를 반드시 사용해야 하는 경우
+// (1) 멤버 데이터로 상수나 참조 멤버 데이터로 있는 경우
+class Object
+{
+	const int c;
+	int& r;
+public:
+	Object(int n, int& x) // 상수는 값 변경 불가하니까 함수 구현에서 대입 불가
+		: c{ n }, r{ x }   // 이렇게 해야함
+	{
+		// c = n; 
+		// r = x; 
+	}
+};
+
+// (2) 디폴트 생성자가 없는 타입이 멤버로 있는 경우
+class Point
+{
+	int x;
+	int y;
+public:
+	/*Point()
+	{
+
+	}
+	*/
+	Point(int a, int b) : x{ a }, y{ b }
+	{
+
+	}
+};
+
+class Rect
+{
+	Point leftTop;
+	Point rightBottom;
+public:
+	Rect(int x1, int y1, int x2, int y2)
+	{
+
+	}
+};
+
+// 초기화 리스트의 주의 사항
+// (1)
+class p
+{
+public:
+	int x;
+	int y;
+
+	p() : y{ 10 }, x{ y }
+	{
+		// 실행은 멤버 데이터 x부터 실행되기 때문에 잘못된 동작을 한다.
+		// 초기화 리스트의 코드를 작성할 때는 멤버 데이터가 선언된 순서대로 초기화 코드를 작성하는 것이 좋다.
+	}
+};
+
+// (2) 선언과 구현 파일로 나누어 클래스를 작성할 때
+// 멤버 초기화 리스트는 구현 파일에 작성
+// 멤버 초기화 리스트 코드에서는 this가 필요없다. (컴파일러가 알아서 처리 해 줌)
+Point::Point(int x, int y) : x{ x }, y{ y }
+{
+
+}
+
+
+// 디폴트 멤버 이니셜 라이저
+// class d의 멤버 변수 초기화는 어떻게 될까?
+class d
+{
+public:
+	int x{ 0 };
+	int y{ 0 };
+
+	d() {}
+	d(int a) : x{ a }  {}
+	d(int a, int b) : x{ a }, y{ b } {}
+};
+
+// 위 코드는 아래와 같이 바뀐다.
+/*
+	int x;
+	int y;
+
+	d() {} : x{0}, y{0} {}
+	d(int a) : x{ a }  y{0} {}
+	d(int a, int b) : x{ a }, y{ b } {}
+*/
+
+
+int main()
+{
+	// (1)
+	int num = 10;
+	Object obj(10, num);
+
+	// (2)
+	Rect r(1, 1, 10, 10); // 객체를 생성하면 leftTop, rightBottom도 생겨야 하는데
+						  // 디폴트 생성자가 없으므로 안된다.
+}
