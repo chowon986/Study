dfc631c243a9d9db776647808663faae3e071f5b
chowon986 <chowon986@gmail.com>
2022-03-17 18:22:14 +0900

    CH04 실습

--- 명품C++Programming/CH4 14/CH4 14.cpp
+++ 명품C++Programming/CH4 14/CH4 14.cpp
@@ -1,22 +1,19 @@
 ﻿#include <iostream>
 #include <string>
-using namespace std;
 #include <stdlib.h>
 #include <time.h>
+using namespace std;
 
 class Player
 {
+private:
 	string name;
-
-	Player()
-	{
-	}
-
 public:
 	void setName(string name)
 	{
 		this->name = name;
 	}
+
 
 	string getName()
 	{
@@ -26,53 +23,64 @@ public:
 
 class GamblingGame
 {
-	Player* p;
+private:
+	Player* p = new Player[2];
 public:
-	void Game()
+	GamblingGame()
+	{
+		cout << "***** 겜블링 게임을 시작합니다. *****" << endl;
+		srand((unsigned)time(0));
+	}
+
+	~GamblingGame()
+	{
+		delete[] p;
+	}
+
+	void setGame()
 	{
 		string name;
-		cout << "***** 겜블링 게임을 시작합니다. *****" << endl << "첫번째 선수 이름>>";
+		cout << "첫번째 선수 이름>>";
 		cin >> name;
+		p[0].setName(name);
+		cout << "두번째 선수 이름>>";
+		cin >> name;
+		p[1].setName(name);
+	}
 
-		for (int i = 0; i < 2; i++)
+	void playGame()
+	{
+		while (1)
 		{
-			cin >> name;
-			p[i].setName(name);
-		}
-
-		char a[1];
-		while (true)
-		{
+			char a[1] = {0};
 			for (int i = 0; i < 2; i++)
 			{
-				cout << p[i].getName() << ":";
-				cin.get(a, 1, '\n');
-				int a = 0;
-				int b = 2;
-				int n = rand() % (b - a + 1) + a;
-				int Random[3] = { n, n, n };
-
-				for (int i = 0; i < 3; i++)
+				cout << p[i].getName() << ": <Enter>" << endl;
+				cin.getline(a, 2, '\n');
+				int r[3];
+				cout << "\t\t";
+				for (int j = 0; j < 3; j++)
 				{
-					cout << Random[i];
+					r[j] = rand() % 3;
+					cout << r[j] << '\t';
 				}
-				if (Random[i] == Random[i + 1] && Random[i + 1] == Random[i + 2])
+				if (r[0] == r[1] && r[1] == r[2])
 				{
 					cout << p[i].getName() << "님 승리!!" << endl;
+					return;
 				}
 				else
 				{
-					cout << "아쉽군요!";
+					cout << "아쉽군요!" << endl;;
 				}
 			}
 		}
 	}
-
 };
 
 int main()
 {
-	srand((unsigned)time(0));
 	GamblingGame a;
-	a.Game();
+	a.setGame();
+	a.playGame();
 }
