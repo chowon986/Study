ac19510f3364806098317e2d87179509f117accc
chowon986 <chowon986@gmail.com>
2022-03-18 12:48:55 +0900

    01 입출력과 사칙연산

---
+++ 백준 알고리즘/08 A나누기B/08 A나누기B.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    cin >> a >> b;
+    cout.precision(9);
+    cout << fixed << (double)a / b;
+}
+
+// cout.precision(/*숫자*/)
+// 자리수를 맞추어 출력하기 위한 용도
+// 숫자 자리수까지 출력하도록 한다.
+
+// fixed
+// 소수점을 고정시켜 표현하겠다
