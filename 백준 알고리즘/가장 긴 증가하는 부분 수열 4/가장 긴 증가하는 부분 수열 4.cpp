8c6ec288dcaaca488f44859f7b1634baa902512b
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-05-04 16:59:12 +0900

    DP - 가장 긴 증가하는 수열 4, 제곱수의 합, 퇴사, 합분해

---
+++ 백준 알고리즘/가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int A[1001];
+int D[1001];
+int V[1001];
+
+int main()
+{
+	int Cnt;
+	cin >> Cnt;
+
+	for (int i = 1; i <= Cnt; i++)
+	{
+		cin >> A[i];
+		D[i] = 1;
+	}
+
+	if (Cnt == 1)
+	{
+		cout << 1 << '\n';
+		cout << A[1];
+		return 0;
+	}
+
+	int Max = 0;
+	int MaxIndex = 0;
+
+	for (int i = 2; i <= Cnt; i++)
+	{
+		for (int j = i - 1; j > 0; j--)
+		{
+			if (A[i] > A[j])
+			{
+				if (D[i] < D[j] + 1)
+				{
+					D[i] = D[j] + 1;
+					V[i] = j;
+				}
+			}
+		}
+
+		if (D[i] > Max)
+		{
+			Max = D[i];
+			MaxIndex = i;
+		}
+	}
+
+	cout << Max << '\n';
+
+	vector<int> Numbers;
+	while (true)
+	{
+		Numbers.push_back(A[MaxIndex]);
+		MaxIndex = V[MaxIndex];
+		if (MaxIndex == 0)
+		{
+			while (Numbers.size() != 0)
+			{
+				cout << Numbers.back() << ' ';
+				Numbers.pop_back();
+			}
+
+			return 0;
+		}
+	}
+}
---
+++ 백준 알고리즘/퇴사/퇴사.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int Cnt;
+	cin >> Cnt;
+
+	vector<int> T(Cnt + 1, 0);
+	vector<int> P(Cnt + 1, 0);
+	vector<int> D(Cnt + 1, 0);
+
+	for (int i = 1; i <= Cnt; i++)
+	{
+		int Num;
+		cin >> Num;
+		T[i] = Num;
+
+		cin >> Num;
+		P[i] = Num;
+	}
+
+	for (int i = Cnt; i > 0; i--)
+	{
+		if (i + T[i] > Cnt +1)
+			continue;
+
+		int Sum = 0;
+
+		if (T[i] == 1)
+			D[i] = P[i];
+		else
+		{
+			for (int j = i + 1; j < i + T[i]; j++)
+			{
+				Sum += D[j];
+			}
+
+			if (P[i] > Sum)
+				D[i] = P[i] - Sum;
+			else
+			{
+				D[i] = 0;
+			}
+		}
+	 }
+
+	int Sum = 0;
+	for (int Value : D)
+	{
+		Sum += Value;
+	}
+
+	cout << Sum;
+}
