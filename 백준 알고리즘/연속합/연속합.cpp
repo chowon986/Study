2db1157e0f6d9e43410c2ed7b1e6e4ef5e908639
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-05-04 14:15:50 +0900

    1912 연속합

---
+++ 백준 알고리즘/연속합/연속합.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int A[100001];
+int D[100001];
+
+int main()
+{
+	int Cnt;
+	cin >> Cnt;
+
+	for (int i = 1; i <= Cnt; i++)
+	{
+		int N;
+		cin >> N;
+		A[i] = N;
+	}
+
+	D[1] = A[1];
+
+	int Max = -1001;
+	for (int i = 1; i <= Cnt; i++)
+	{
+		D[i] = max(D[i - 1] + A[i], A[i]);
+
+		if (D[i] > Max)
+		{
+			Max = D[i];
+		}
+	}
+
+	cout << Max;
+}
