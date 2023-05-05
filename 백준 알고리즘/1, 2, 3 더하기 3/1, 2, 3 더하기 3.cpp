c1913d2d55752c032ef58e535b530a5646303f8c
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-05-05 16:23:06 +0900

    김초원 : 1, 2, 3 더하기 3, RGB거리

---
+++ 백준 알고리즘/1, 2, 3 더하기 3/1, 2, 3 더하기 3.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+
+#define MAX 1000001
+#define MOD 1000000009
+
+using namespace std;
+
+int main()
+{
+	int Cnt;
+	cin >> Cnt;
+
+	vector<int> D(MAX, 0);
+
+	D[0] = 1;
+
+	for (int i = 1; i < MAX; i++)
+	{
+		if (i - 1 >= 0)
+			D[i] += D[i - 1];
+
+		if (i - 2 >= 0)
+			D[i] += D[i - 2];
+
+		D[i] %= MOD;
+
+		if (i - 3 >= 0)
+			D[i] += D[i - 3];
+
+		D[i] %= MOD;
+
+	}
+
+	for (int i = 0; i < Cnt; i++)
+	{
+		int Num;
+		cin >> Num;
+		cout << D[Num] << '\n';
+	}
+}
---
+++ 백준 알고리즘/RGB거리/RGB거리.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int Cnt;
+	cin >> Cnt;
+
+	int Cost[3];
+	int House[1001][3];
+
+	House[0][0] = 0;
+	House[0][1] = 0;
+	House[0][2] = 0;
+
+	for (int i = 1; i <= Cnt; i++)
+	{
+		cin >> Cost[0] >> Cost[1] >> Cost[2];
+		House[i][0] = min(House[i - 1][1], House[i - 1][2]) + Cost[0];
+		House[i][1] = min(House[i - 1][0], House[i - 1][2]) + Cost[1];
+		House[i][2] = min(House[i - 1][0], House[i - 1][1]) + Cost[2];
+	}
+
+	cout << min(House[Cnt][2], min(House[Cnt][0], House[Cnt][1]));
+}
