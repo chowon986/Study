ce16ea2119e31e3847afa15e5bfa2df947471418
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-05-09 19:46:30 +0900

    김초원 : 포도주 시식

---
+++ 백준 알고리즘/포도주 시식/포도주 시식.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int D[10001][4];
+int A[10001];
+
+int main()
+{
+	int Cnt;
+	cin >> Cnt;
+
+	for (int i = 1; i <= Cnt; i++)
+	{
+		int N;
+		cin >> N;
+		A[i] = N;
+	}
+
+	for (int i = 1; i <= Cnt; i++)
+	{
+		D[i][0] = max(D[i - 1][0], max(D[i - 1][1], D[i - 1][2]));
+		D[i][1] = D[i - 1][0] + A[i];
+		D[i][2] = D[i - 1][1] + A[i];
+	}
+
+	int a = 0;
+}
