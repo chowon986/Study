eaf9b37d9db554e05cf4fd98ed879c5e6d03b2dd
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-05-09 22:40:39 +0900

    김초원 : 가장 큰 증가하는 부분 수열

---
+++ 백준 알고리즘/가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cpp
@@ -0,0 +1,48 @@
+﻿/*
+점화식
+D[i] = i번까지 있을때 최대 값
+D[i] = A[1];
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> A(1001, 1);
+vector<int> D(1001, 0);
+
+int main()
+{
+	int Cnt;
+	cin >> Cnt;
+
+	for (int i = 1; i <= Cnt; i++)
+	{
+		int N;
+		cin >> N;
+		A[i] = N;
+		D[i] = N;
+	}
+
+	for (int i = 1; i <= Cnt; i++)
+	{
+		for (int j = i; j > 0; j--)
+		{
+			if (A[i] > A[j])
+			{
+				D[i] = max(D[i], D[j] + A[i]);
+			}
+		}
+	}
+
+	int Max = -1;
+	for (int i = 1; i <= Cnt; i++)
+	{
+		if (Max < D[i])
+		{
+			Max = D[i];
+		}
+	}
+
+	cout << Max;
+}
