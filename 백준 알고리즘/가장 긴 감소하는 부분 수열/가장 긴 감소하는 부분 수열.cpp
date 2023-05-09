2bdc663dc29a977178ba4dad31b0379804b7f13f
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-05-10 00:01:34 +0900

    김초원 : 가장 긴 감소하는 부분 수열, 가장 긴 바이토닉 부분 수열

---
+++ 백준 알고리즘/가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int Cnt;
+	cin >> Cnt;
+
+	vector<int> D(Cnt + 1, 0);
+	vector<int> A(Cnt + 1, 0);
+
+	for (int i = Cnt; i > 0; i--)
+	{
+		int N;
+		cin >> N;
+		D[i] = N;
+		A[i] = 1;
+	}
+
+
+	for (int i = 1; i <= Cnt; i++)
+	{
+		for (int j = i - 1; j > 0; j--)
+		{
+			if (D[i] > D[j])
+			{
+				A[i] = max(A[i], A[j] + 1);
+			}
+		}
+	}
+
+	int Max = -1;
+	for (int i = 1; i <= Cnt; i++)
+	{
+		if (Max < A[i])
+		{
+			Max = A[i];
+		}
+	}
+
+	cout << Max;
+}
---
+++ 백준 알고리즘/가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int Cnt;
+	cin >> Cnt;
+
+	vector<int> Numbers(Cnt + 1, 0);
+	vector<int> U(Cnt + 1, 0);
+	vector<int> D(Cnt + 1, 0);
+
+	for (int i = 1; i <= Cnt; i++)
+	{
+		int N;
+		cin >> N;
+		Numbers[i] = N;
+		U[i] = 1;
+		D[i] = 1;
+	}
+
+	for (int i = 1; i <= Cnt; i++)
+	{
+		for (int j = i - 1; j > 0; j--)
+		{
+			if (Numbers[i] > Numbers[j])
+			{
+				U[i] = max(U[i], U[j] + 1);
+			}
+		}
+	}
+
+	for (int i = Cnt - 1; i > 0; i--)
+	{
+		for (int j = i + 1; j <= Cnt; j++)
+		{
+			if (Numbers[i] > Numbers[j])
+			{
+				D[i] = max(D[i], D[j] + 1);
+			}
+		}
+	}
+
+	int Max = -1;
+	for (int i = 1; i <= Cnt; i++)
+	{
+		if (Max < U[i] + D[i])
+		{
+			Max = U[i] + D[i];
+		}
+	}
+
+	cout << Max - 1;
+}
