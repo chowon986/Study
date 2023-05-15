e40b740e9b18c457ccfb860815047e69cf4c8072
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-05-15 18:12:09 +0900

    김초원 : 브루트포스 일곱난쟁이

---
+++ 백준 알고리즘/일곱난쟁이/일곱난쟁이.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<int> Heights;
+
+    for (int i = 0; i < 9; i++)
+    {
+        int Temp;
+        cin >> Temp;
+        Heights.push_back(Temp);
+    }
+
+    sort(Heights.begin(), Heights.end());
+
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = i + 1; j < 9; j++)
+        {
+            int Sum = 0;
+            for (int k = 0; k < 9; k++)
+            {
+                if (k != i && k != j)
+                {
+                    Sum += Heights[k];
+
+                    if (Sum > 100)
+                        break;
+                }
+            }
+
+            if (Sum == 100)
+            {
+                for (int l = 0; l < 9; l++)
+                {
+                    if (l != i && l != j)
+                        cout << Heights[l] << '\n';
+                }
+
+                return 0;
+            }
+        }
+    }
+}
