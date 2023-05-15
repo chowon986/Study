947e564ae6238ee793a5493e4d4969f37a551579
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-05-15 19:17:56 +0900

    김초원 : 브루트포스 날짜 계산

---
+++ 백준 알고리즘/날짜 계산/날짜 계산.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int E, S, M;
+
+	cin >> E >> S >> M;
+
+	vector<int> Year(3, 1);
+
+	int Count = 1;
+	while (Year[0] != E || Year[1] != S || Year[2] != M)
+	{
+		++Year[0];
+		if (Year[0] == 16)
+			Year[0] = 1;
+
+		++Year[1];
+		if (Year[1] == 29)
+			Year[1] = 1;
+
+		++Year[2];
+		if (Year[2] == 20)
+			Year[2] = 1;
+
+		++Count;
+	}
+
+	cout << Count;
+}
