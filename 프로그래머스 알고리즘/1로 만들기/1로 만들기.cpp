cd61837b122d13c9dc3e2b6499e730bcf7029dec
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-07-14 23:33:40 +0900

    김초원 : 프로그래머스 코딩 기초 트레이닝

---
+++ 프로그래머스 알고리즘/1로 만들기/1로 만들기.cpp
@@ -0,0 +1,30 @@
+﻿// 1로 만들기.cpp :https://school.programmers.co.kr/learn/courses/30/lessons/181880
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list)
+{
+    int size = num_list.size();
+    int count = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        while (num_list[i] != 1)
+        {
+            ++count;
+            if (num_list[i] % 2 != 0)
+                --num_list[i];
+
+            num_list[i] /= 2;
+        }
+    }
+    return count;
+}
+
+int main()
+{
+
+}
---
+++ 프로그래머스 알고리즘/조건에 맞게 수열 변환하기2/조건에 맞게 수열 변환하기2.cpp
@@ -0,0 +1,33 @@
+﻿// 조건에 맞게 수열 변환하기2.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/181881
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> arr)
+{
+    vector<int> prevArr;
+    int size = arr.size();
+    int count = 0;
+
+    while (prevArr != arr)
+    {
+        ++count;
+        prevArr = arr;
+        for (int i = 0; i < size; i++)
+        {
+            if (arr[i] >= 50 && arr[i] % 2 == 0)
+                arr[i] /= 2;
+            else if (arr[i] < 50 && arr[i] % 2 != 0)
+                arr[i] = arr[i] * 2 + 1;
+        }
+    }
+
+    return count - 1;
+}
+
+int main()
+{
+
+}
