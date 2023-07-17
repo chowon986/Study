0ce79bb4256d664eb4657ccd5d8853cd669702dd
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-07-17 18:32:37 +0900

    김초원 : 프로그래머스 코딩 기초 트레이닝

---
+++ 프로그래머스 알고리즘/날짜 비교하기/날짜 비교하기.cpp
@@ -0,0 +1,29 @@
+﻿// 날짜 비교하기.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/181838
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> date1, vector<int> date2)
+{
+    if (date1[0] < date2[0])
+        return 1;
+    else if (date1[0] > date2[0])
+        return 0;
+    else if (date1[1] < date2[1])
+        return 1;
+    else if (date1[1] > date2[1])
+        return 0;
+    else if (date1[2] < date2[2])
+        return 1;
+    else if (date1[2] > date2[2])
+        return 0;
+
+    return 0;
+}
+
+int main()
+{
+
+}
---
+++ 프로그래머스 알고리즘/문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.cpp
@@ -0,0 +1,41 @@
+﻿// 문자열이 몇 번 등장하는지 세기.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/181871
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string myString, string pat)
+{
+    int size = myString.size();
+    int patSize = pat.size();
+    int answer = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        int k = 0;
+        int count = 0;
+        if (myString[i] == pat[k])
+        {
+            int lastIdx = (i + patSize) > size ? size : (i + patSize);
+            for (int j = i; j < lastIdx; j++)
+            {
+                if (myString[j] == pat[k++])
+                    ++count;
+            }
+
+            if(count == patSize)
+                ++answer;
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    string myString = "banana";
+    string pat = "ana";
+
+    solution(myString, pat);
+}
