a2abd06e50e9a229fb34eef8c91b2b97842c4862
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-07-20 14:25:13 +0900

    김초원 : 프로그래머스 코딩 기초 트레이닝

---
+++ 프로그래머스 알고리즘/문자열 묶기/문자열 묶기.cpp
@@ -0,0 +1,34 @@
+﻿// 문자열 묶기.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/181855
+
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<string> strArr)
+{
+    int strSizeArr[31];
+    int size = strArr.size();
+
+    for (int i = 0; i < size; i++)
+    {
+        ++strSizeArr[strArr[i].size()];
+    }
+
+    int max = -1;
+
+    for (int i = 0; i < 31; i++)
+    {
+        if (strSizeArr[i] > max)
+            max = strSizeArr[i];
+    }
+
+    return max;
+}
+
+int main()
+{
+    vector<string> strArr = { "a","bc","d","efg","hi" };
+    solution(strArr);
+}
---
+++ 프로그래머스 알고리즘/배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.cpp
@@ -0,0 +1,35 @@
+﻿// 배열의 길이를 2의 거듭제곱으로 만들기.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/181857
+
+#include <string>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+    int base = 2;
+    int size = arr.size();
+
+    if (size == 1)
+        return arr;
+
+    while (base < size)
+    {
+        base *= 2;
+    }
+
+    base -= size;
+
+    for (int i = 0; i < base; i++)
+    {
+        arr.push_back(0);
+    }
+
+    return arr;
+}
+
+int main()
+{
+
+}
