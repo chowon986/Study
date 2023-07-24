a3120bae60d708f856e8471a9698c2c977f85249
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-07-24 22:26:45 +0900

    김초원 : 프로그래머스 코딩 기초 트레이닝

---
+++ 프로그래머스 알고리즘/그림 확대/그림 확대.cpp
@@ -0,0 +1,37 @@
+﻿// 그림 확대.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/181836
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> picture, int k)
+{
+    vector<string> answer;
+    int size = picture.size();
+    answer.resize(size * k);
+    int q = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        int tempSize = picture[i].size();
+        for (int j = 0; j < tempSize; j++)
+        {
+            for (int a = 0; a < k; a++)
+                answer[q] += picture[i][j];
+        }
+
+        for (int p = 0; p < k; p++)
+        {
+            answer[q + p] = answer[q];
+        }
+        q += k;
+    }
+
+    return answer;
+}
+
+int main()
+{
+
+}
