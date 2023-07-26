a1320da0a2de3e092a77ac5ca04c258dc6eaf142
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-07-26 15:30:17 +0900

    김초원 : 프로그래머스 코딩 기초 트레이닝

---
+++ 프로그래머스 알고리즘/실패율/실패율.cpp
@@ -0,0 +1,59 @@
+﻿// 실패율.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/42889
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+bool cmp(pair<int, float>& a, pair<int, float>& b)
+{
+    if (a.second == b.second)
+    {
+        return a.first < b.first;
+    }
+
+    return a.second > b.second;
+}
+
+std::vector<int> solution(int N, vector<int> stages)
+{
+    float count;
+    int TTLNum = stages.size();
+    map<int, float> failures;
+
+    for (int i = 1; i <= N; i++)
+    {
+        count = 0;
+
+        for (int j : stages)
+        {
+            if (i == j)
+                ++count;
+        }
+
+        if (count == 0)
+            failures.insert(make_pair(i, 0));
+        else
+            failures.insert(make_pair(i, (count / TTLNum)));
+
+        TTLNum -= count;
+    }
+
+    vector<pair<int, float>> v(failures.begin(), failures.end());
+    vector<int> answer;
+    sort(v.begin(), v.end(), cmp);
+
+    for (int i = 0; i < N; i++)
+    {
+        answer.push_back(v[i].first);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution(2, { 1, 1, 1, 1 });
+}
---
+++ 프로그래머스 알고리즘/카드 뭉치/카드 뭉치.cpp
@@ -0,0 +1,43 @@
+﻿// 카드 뭉치.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/159994
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal)
+{
+    int size = goal.size();
+    int j = 0;
+    int k = 0;
+    int count = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (goal[i] == cards1[j])
+        {
+            ++count;
+            ++j;
+        }
+
+        else if (goal[i] == cards2[k])
+        {
+            ++count;
+            ++k;
+        }
+
+        else
+            return "No";
+    }
+    
+    return "Yes";
+}
+
+int main()
+{
+    vector<string> cards1 = { "you", "now", "me" };
+    vector<string> cards2 = { "i", "see", "to" };
+    vector<string> goal = { "i", "see", "now", "me" };
+
+    solution(cards1, cards2, goal);
+}
