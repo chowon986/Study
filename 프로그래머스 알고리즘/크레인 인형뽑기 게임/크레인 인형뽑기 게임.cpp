593528178e62b85e95c6e3427ee988321b4d8ca3
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-07-27 18:49:04 +0900

    김초원 : 프로그래머스 코딩 기초 트레이닝

---
+++ 프로그래머스 알고리즘/크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -0,0 +1,41 @@
+﻿// 크레인 인형뽑기 게임.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/64061
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, vector<int> moves)
+{
+    int size = board.size();
+    int moveCount = moves.size();
+    vector<int> dolls;
+    int answer = 0;
+
+    for (int i = 0; i < moveCount; i++)
+    {
+        int idx = moves[i] - 1;
+
+        for (int j = 0; j < size; j++)
+        {
+            if (board[j][idx] != 0)
+            {
+                if (dolls.size() == 0 || dolls.back() != board[j][idx])
+                {
+                    dolls.push_back(board[j][idx]);
+                    board[j][idx] = 0;
+                    break;
+                }
+                else if (dolls.back() == board[j][idx])
+                {
+                    dolls.pop_back();
+                    board[j][idx] = 0;
+                    ++answer;
+                    break;
+                }
+            }
+        }
+    }
+
+    return answer * 2;
+}
