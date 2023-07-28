43969e952395955a831aa32afea10b22da544204
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-07-28 14:10:16 +0900

    김초원 : 프로그래머스 코딩 기초 트레이닝

---
+++ 프로그래머스 알고리즘/신규 아이디 추천/신규 아이디 추천.cpp
@@ -0,0 +1,91 @@
+﻿// 신규 아이디 추천.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/72410
+
+#include <string>
+#include <cctype>
+#include <vector>
+
+using namespace std;
+
+string solution(string new_id)
+{
+    int size = new_id.size();
+    string answer = "";
+    vector<char> recommendedID;
+
+    for (int i = 0; i < size; i++)
+    {
+        char temp = tolower(new_id[i]);
+
+        if ((temp >= 97 && temp <= 122) || 0 != isdigit(temp) || temp == '-' || temp == '_' || temp == '.')
+        {
+            if (recommendedID.size() == 0)
+            {
+                if (temp == '.')
+                    continue;
+                else
+                {
+                    recommendedID.push_back(temp);
+                    continue;
+                }
+            }
+
+            if (recommendedID.back() == '.' && temp == '.')
+                continue;
+            
+            recommendedID.push_back(temp);
+        }
+    }
+
+    if (recommendedID.size() != 0)
+    {
+        while (recommendedID.back() == '.')
+            recommendedID.pop_back();
+    }
+
+    int newSize = recommendedID.size();
+
+    if (newSize == 0)
+    {
+        answer += 'a';
+    }
+
+    if (newSize > 15)
+    {
+        vector<char> newWord;
+
+        for (int i = 0; i < 15; i++)
+        {
+            newWord.push_back(recommendedID[i]);
+        }
+
+        while (newWord.back() == '.')
+            newWord.pop_back();
+
+        int newWordSize = newWord.size();
+        for (int i = 0; i < newWordSize; i++)
+            answer += newWord[i];
+    }
+
+    else if (newSize < 3)
+    {
+        char lastWord = recommendedID.back();
+
+        while (recommendedID.size() != 3)
+            recommendedID.push_back(lastWord);
+
+        for (int i = 0; i < 3; i++)
+            answer += recommendedID[i];
+    }
+    else
+    {
+        for (int i = 0; i < newSize; i++)            
+            answer += recommendedID[i];
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution("=.=");
+}
