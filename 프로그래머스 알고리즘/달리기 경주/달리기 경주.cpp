196c2474e96f95e73ce170db2900e7bf256ca96c
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-07-29 00:02:18 +0900

    김초원 : 프로그래머스 코딩 기초 트레이닝

---
+++ 프로그래머스 알고리즘/달리기 경주/달리기 경주.cpp
@@ -0,0 +1,48 @@
+﻿// 달리기 경주.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/178871
+
+/*
+<이름, 순위> 값을 가지는 map
+<순위, 이름> 값을 가지는 map
+*/
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<string> solution(vector<string> players, vector<string> callings)
+{
+    map<string, int> playersNameNum;
+    map<int, string> playersNumName;
+    int size = players.size();
+
+    for (int i = 0; i < size; i++)
+    {
+        playersNameNum.insert(make_pair(players[i], i + 1));
+        playersNumName.insert(make_pair(i + 1, players[i]));
+    }
+
+    int callingSize = callings.size();
+
+    for (int i = 0; i < callingSize; i++)
+    {
+        string prevPlayerName = playersNumName[playersNameNum[callings[i]] - 1];
+
+        swap(playersNameNum[callings[i]], playersNameNum[prevPlayerName]);
+        swap(playersNumName[playersNameNum[callings[i]]], playersNumName[playersNameNum[prevPlayerName]]); 
+    }
+
+    vector<string> answer;
+    for (int i = 1; i <= size; i++)
+        answer.push_back(playersNumName[i]);
+
+    return answer;
+}
+
+int main()
+{
+    vector<string> players = { "mumu", "soe", "poe", "kai", "mine" };
+    vector<string> callings = { "kai", "kai", "mine", "mine" };
+
+    solution(players, callings);
+}
---
+++ 프로그래머스 알고리즘/바탕화면 정리/바탕화면 정리.cpp
@@ -0,0 +1,42 @@
+﻿// 바탕화면 정리.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/161990
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> wallpaper)
+{
+    int size = wallpaper.size();
+    int startx = 51;
+    int starty = 51;
+    int endx = -1;
+    int endy = -1;
+
+    for (int i = 0; i < size; i++)
+    {
+        int curSize = wallpaper[i].size();
+
+        for (int j = 0; j < curSize; j++)
+        {
+            if (wallpaper[i][j] == '#')
+            {
+                if (i < startx) startx = i;
+                if (j < starty) starty = j;
+                if (i > endx) endx = i;
+                if (j > endy) endy = j;
+            }
+        }
+    }
+    vector<int> answer;
+    answer.push_back(startx);
+    answer.push_back(starty);
+    answer.push_back(endx + 1);
+    answer.push_back(endy + 1);
+    return answer;
+}
+
+int main()
+{
+
+}
