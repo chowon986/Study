0c69dbd4e922a945c6e427a733cc61efd9fb09e6
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-07-31 19:01:10 +0900

    김초원 : 프로그래머스 코딩 기초 트레이닝

---
+++ 프로그래머스 알고리즘/짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,35 @@
+﻿// 짝지어 제거하기.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/12973
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(string s)
+{
+    int answer = -1;
+    vector<char> words;
+
+    int size = s.size();
+
+    for (int i = 0; i < size; i++)
+    {
+        if (words.size() > 0)
+        {
+            if (words.back() == s[i])
+                words.pop_back();
+            else
+                words.push_back(s[i]);
+        }
+        else
+            words.push_back(s[i]);
+    }
+
+
+    return words.size() > 0 ? 0 : 1;
+}
+
+int main()
+{
+
+}
