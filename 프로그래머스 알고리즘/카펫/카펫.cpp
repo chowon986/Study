b85cdaaf82add12c277a49ddb7514b7b561b75e4
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-07-31 19:27:10 +0900

    김초원 : 프로그래머스 코딩 기초 트레이닝

---
+++ 프로그래머스 알고리즘/카펫/카펫.cpp
@@ -0,0 +1,46 @@
+﻿// 카펫.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/42842
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow)
+{
+    int totalCount = yellow + brown;
+    vector<int> answer;
+
+    for (int i = 2; i <= brown; i++)
+    {
+        if (i - 2 == yellow)
+        {
+            if (i * 2 + 2 == brown)
+            {
+                answer.push_back(i);
+                answer.push_back(3);
+                return answer;
+            }
+        }
+    }
+
+    for (int i = 2; i <= brown; i++)
+    {
+        if (yellow % i == 0)
+        {
+            int height = yellow / i;
+            if (i * 2 + ((height + 2) * 2) == brown)
+            {
+                answer.push_back(height + 2);
+                answer.push_back(i + 2);
+                return answer;
+            }
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+
+}
