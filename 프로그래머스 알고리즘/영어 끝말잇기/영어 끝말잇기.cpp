6def8efe5e4bc5b78169b98b5adf5a80732c46db
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-08-01 09:46:31 +0900

    김초원 : 프로그래머스 코딩 기초 트레이닝

---
+++ 프로그래머스 알고리즘/영어 끝말잇기/영어 끝말잇기.cpp
@@ -0,0 +1,71 @@
+﻿// 영어 끝말잇기.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/12981
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(int n, vector<string> words)
+{
+    vector<int> answer;
+    vector<string> allWords;
+
+    int curPeopleNum = 1;
+    int idx = 0;
+    int turn = 1;
+    while (true)
+    {
+        if (curPeopleNum == n + 1)
+        {
+            curPeopleNum = 1;
+            ++turn;
+        }
+        if (idx >= words.size())
+            break;
+
+        if (allWords.size() >= 1)
+        {
+            string curWord = allWords.back();
+            int lastIdx = curWord.size() - 1;
+            if (curWord[lastIdx] == words[idx].front())
+            {
+                if (find(allWords.begin(), allWords.end(), words[idx]) == allWords.end())
+                {
+                    allWords.push_back(words[idx]);
+                    ++idx;
+                    ++curPeopleNum;
+                }
+                else
+                {
+                    answer.push_back(curPeopleNum);
+                    answer.push_back(turn);
+                    return answer;
+                }
+            }
+            else
+            {
+                answer.push_back(curPeopleNum);
+                answer.push_back(turn);
+                return answer;
+            }
+        }
+        else
+        {
+            allWords.push_back(words[idx]);
+            ++idx;
+            ++curPeopleNum;
+        }
+    }
+
+    answer.push_back(0);
+    answer.push_back(0);
+
+    return answer;
+}
+
+int main()
+{
+
+}
