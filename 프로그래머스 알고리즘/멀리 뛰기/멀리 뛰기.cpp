c176ec5b88b5dbd34896a0384c4d875afd6e0277
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-08-02 16:52:09 +0900

    김초원 : 프로그래머스 코딩 기초 트레이닝

---
+++ 프로그래머스 알고리즘/멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,25 @@
+﻿// 멀리 뛰기.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/12914
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n)
+{
+    int d[2001] = { 0, };
+    d[0] = 1;
+    d[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+        d[i] = (d[i - 1] + d[i - 2]) % 1234567;
+
+    long long answer = d[n];
+
+    return answer;
+}
+
+int main()
+{
+
+}
