00c139dfd958fbe62da664482d640a3ec45ca54c
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-03 13:53:53 +0900

    김초원 : 프로그래머스 코딩 기초 트레이닝

---
+++ 프로그래머스 알고리즘/괄호 회전하기/괄호 회전하기.cpp
@@ -0,0 +1,60 @@
+﻿// 괄호 회전하기.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/76502
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s)
+{
+    int size = s.size();
+    int count = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        vector<char> curWord;
+        for (int j = i; j < i + size; j++)
+        {
+            if (curWord.size() == 0)
+                curWord.push_back(s[j]);
+            else
+            {
+                if (curWord.back() == '(')
+                {
+                    if (s[j] == ')')
+                        curWord.pop_back();
+                    else
+                        curWord.push_back(s[j]);
+                }
+                else if (curWord.back() == '[')
+                {
+                    if (s[j] == ']')
+                        curWord.pop_back();
+                    else
+                        curWord.push_back(s[j]);
+                }
+                else if (curWord.back() == '{')
+                {
+                    if (s[j] == '}')
+                        curWord.pop_back();
+                    else
+                        curWord.push_back(s[j]);
+                }
+                else
+                    curWord.push_back(s[j]);
+            }
+        }
+
+        if (curWord.size() == 0)
+            ++count;
+
+        s += s[i];
+    }
+
+    return count;
+}
+
+int main()
+{
+    solution("[)(]");
+}
