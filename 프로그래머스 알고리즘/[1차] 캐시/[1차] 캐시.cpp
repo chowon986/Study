8fc14b17cbee787c3a10e11f6a0b497874992efe
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-06 10:13:23 +0900

    김초원 : 프로그래머스 코딩 기초 트레이닝

---
+++ 프로그래머스 알고리즘/[1차] 캐시/[1차] 캐시.cpp
@@ -0,0 +1,56 @@
+﻿// [1차] 캐시.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/17680
+
+#include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int solution(int cacheSize, vector<string> cities)
+{
+    // 예외 처리
+    if (cacheSize == 0)
+        return cities.size() * 5;
+
+    list<string> value;
+    value.resize(cacheSize, "");
+    int size = cities.size();
+    int answer = 0;
+
+    // 소문자로 변경
+    for (int i = 0; i < size; i++)
+    {
+        int tempSize = cities[i].size();
+        for (int j = 0; j < tempSize; j++)
+        {
+            if (cities[i][j] >= 'A' && cities[i][j] <= 'Z')
+                cities[i][j] = cities[i][j] + 32;
+        }
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		if (find(value.begin(), value.end(), cities[i]) == value.end())
+		{
+			value.pop_back();
+			value.push_front(cities[i]);
+			answer += 5;
+		}
+		else // 현재 cache에 값이 있다면
+		{
+			list<string>::iterator curit = find(value.begin(), value.end(), cities[i]);
+			value.erase(curit);
+			value.push_front(cities[i]);
+			answer += 1;
+		}
+	}
+
+	return answer;
+}
+
+int main()
+{
+    solution(3, { "a", "b", "c", "a", "b", "d", "c" });
+}
