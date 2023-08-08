c995d4eefafdbaee706b9f0669ae750d9abc97de
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-08 19:54:23 +0900

    김초원 : 프로그래머스 코딩 기초 트레이닝

---
+++ 프로그래머스 알고리즘/전화번호 목록/전화번호 목록.cpp
@@ -0,0 +1,24 @@
+﻿// 전화번호 목록.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/42577
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book)
+{
+    sort(phone_book.begin(), phone_book.end());
+
+    for (int i = 0; i < phone_book.size() - 1; i++)
+    {
+        if (phone_book[i] == phone_book[i + 1].substr(0, phone_book[i].size()))
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+
+}
