7882fe1f1b4004c4129309f50cb33994f9daf5e1
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-10 11:57:30 +0900

    김초원 : 프로그래머스 코딩 기초 트레이닝

---
+++ 프로그래머스 알고리즘/주식가격/주식가격.cpp
@@ -0,0 +1,34 @@
+﻿// 주식가격.cpp : https://school.programmers.co.kr/learn/courses/30/lessons/42584
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices)
+{
+    vector<int> answer;
+    int size = prices.size();
+    for (int i = 0; i < size; i++)
+    {
+        int count = 0;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (prices[i] <= prices[j])
+                ++count;
+            else
+            {
+                ++count;
+                break;
+            }
+        }
+        answer.push_back(count);
+    }
+
+    return answer;
+}
+
+int main()
+{
+
+}
