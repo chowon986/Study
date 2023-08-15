b3a22584fbdd687c39c80d08974279688212ef86
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-08-15 23:05:46 +0900

    김초원 : [수학] 소수 찾기

---
+++ 백준 알고리즘/소수 찾기/소수 찾기.cpp
@@ -0,0 +1,34 @@
+﻿// 소수 찾기.cpp : https://www.acmicpc.net/problem/1978
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int count;
+	cin >> count;
+	int answer = 0;
+
+	for (int i = 0; i < count; i++)
+	{
+		int num;
+		cin >> num;
+
+		bool isPrime = true;
+		for (int j = 2; j < num; j++)
+		{
+			if (num % j == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+
+		if (num == 1) isPrime = false;
+
+		if (isPrime)
+			++answer;
+	}
+
+	cout << answer;
+}
