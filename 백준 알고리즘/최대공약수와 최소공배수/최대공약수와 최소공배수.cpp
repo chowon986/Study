aa74826b27205ed256026d216014a4c23d7299d4
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-08-15 23:01:23 +0900

    김초원 : [수학] 최대공약수와 최소공배수

---
+++ 백준 알고리즘/최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -0,0 +1,33 @@
+﻿// 최대공약수와 최소공배수.cpp : https://www.acmicpc.net/problem/2609
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	int originA = a;
+	int originB = b;
+
+	while (true)
+	{
+		if (a < b)
+		{
+			int temp = b;
+			b = a;
+			a = temp;
+		}
+
+		int temp = b;
+		b = a % b;
+		a = temp;
+
+		if (b == 0)
+		{
+			cout << a << '\n';
+			cout << a * (originA / a) * (originB / a);
+			break;
+		}
+	}
+}
