6e428dcdd7e3c6e1f00ccb50a720194987365fbf
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-08-15 22:55:31 +0900

    김초원 : [수학] 약수의 합

---
+++ 백준 알고리즘/약수의 합/약수의 합.cpp
@@ -0,0 +1,33 @@
+﻿// 약수의 합.cpp : https://www.acmicpc.net/problem/17425
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int max = 1000001;
+	vector <long long> g(max, 1);
+
+	for (int i = 2; i < max; i++)
+	{
+		for (int j = 1; i * j < max; j++)
+			g[i * j] += i;
+	}
+
+	for (int i = 2; i < max; i++)
+		g[i] = g[i] + g[i - 1];
+
+	int count;
+	cin >> count;
+
+	for (int i = 0; i < count; i++)
+	{
+		int num;
+		cin >> num;
+		cout << g[num] << '\n';
+	}
+}
