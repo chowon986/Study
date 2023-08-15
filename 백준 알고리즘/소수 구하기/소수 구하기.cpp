2ec4f5ebbcb6dc60394ef191e07277943ce9246e
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-08-15 23:18:03 +0900

    김초원 : [수학] 소수 구하기

---
+++ 백준 알고리즘/소수 구하기/소수 구하기.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+
+    vector<bool> prime(1000001, 1);
+    prime[1] = 0;
+
+    for (int i = 2; i * i < 1000001; i++)
+    {
+        if (prime[i] == 1)
+        {
+            for (int j = i + i; j < 1000001; j += i)
+                prime[j] = 0;
+        }
+    }
+    
+
+    for (int i = m; i <= n; i++)
+    {
+        if (prime[i] == 1)
+            cout << i << '\n';
+    }
+}
