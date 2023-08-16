6b8ea5aa9333beb34074225ff8ca2030b238c3e9
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-08-16 10:14:01 +0900

    김초원 : [브루트포스] 일곱난쟁이

--- 백준 알고리즘/일곱난쟁이/일곱난쟁이.cpp
+++ 백준 알고리즘/일곱난쟁이/일곱난쟁이.cpp
@@ -1,51 +1,106 @@
-﻿#include <iostream>
+﻿//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//
+//    vector<int> Heights;
+//
+//    for (int i = 0; i < 9; i++)
+//    {
+//        int Temp;
+//        cin >> Temp;
+//        Heights.push_back(Temp);
+//    }
+//
+//    sort(Heights.begin(), Heights.end());
+//
+//    for (int i = 0; i < 8; i++)
+//    {
+//        for (int j = i + 1; j < 9; j++)
+//        {
+//            int Sum = 0;
+//            for (int k = 0; k < 9; k++)
+//            {
+//                if (k != i && k != j)
+//                {
+//                    Sum += Heights[k];
+//
+//                    if (Sum > 100)
+//                        break;
+//                }
+//            }
+//
+//            if (Sum == 100)
+//            {
+//                for (int l = 0; l < 9; l++)
+//                {
+//                    if (l != i && l != j)
+//                        cout << Heights[l] << '\n';
+//                }
+//
+//                return 0;
+//            }
+//        }
+//    }
+//}
+
+#include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+	int count = 9;
+	int sum = 0;
+	vector<int> talls;
 
-    vector<int> Heights;
+	for (int i = 0; i < count; i++)
+	{
+		int value;
+		cin >> value;
 
-    for (int i = 0; i < 9; i++)
-    {
-        int Temp;
-        cin >> Temp;
-        Heights.push_back(Temp);
-    }
+		talls.push_back(value);
+		sum += value;
+	}
 
-    sort(Heights.begin(), Heights.end());
+	sort(talls.begin(), talls.end());
 
-    for (int i = 0; i < 8; i++)
-    {
-        for (int j = i + 1; j < 9; j++)
-        {
-            int Sum = 0;
-            for (int k = 0; k < 9; k++)
-            {
-                if (k != i && k != j)
-                {
-                    Sum += Heights[k];
+	int i = 0;
+	int j = 1;
+	int copySum = sum;
+	while (i < count - 1)
+	{
+		if (j == count)
+		{
+			++i;
+			j = i + 1;
+		}
+		else if (j > count)
+			break;
 
-                    if (Sum > 100)
-                        break;
-                }
-            }
+		copySum -= talls[i];
+		copySum -= talls[j];
 
-            if (Sum == 100)
-            {
-                for (int l = 0; l < 9; l++)
-                {
-                    if (l != i && l != j)
-                        cout << Heights[l] << '\n';
-                }
-
-                return 0;
-            }
-        }
-    }
+		if (copySum == 100)
+		{
+			for (int k = 0; k < count; k++)
+			{
+				if (k != i && k != j)
+					cout << talls[k] << '\n';
+			}
+			break;
+		}
+		else
+		{
+			copySum = sum;
+			++j;
+		}
+	}
 }
