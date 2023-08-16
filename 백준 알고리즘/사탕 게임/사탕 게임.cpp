34f7b72fdb76cb56ddff32452b18937fe1e8dfd3
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-08-16 11:17:56 +0900

    김초원 : [브루트포스] 사탕 게임

--- 백준 알고리즘/사탕 게임/사탕 게임.cpp
+++ 백준 알고리즘/사탕 게임/사탕 게임.cpp
@@ -1,56 +1,160 @@
-﻿#include <iostream>
+﻿//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int CheckMaxCandies(vector<vector<char>> C, int N)
+//{
+//	for (int i = 0; i < N; i++)
+//	{
+//		for (int j = 0; j < N; j++)
+//		{
+//
+//		}
+//	}
+//
+//	return 1;
+//}
+//
+//int main()
+//{
+//	int N;
+//	cin >> N;
+//
+//	vector<vector<char>> Candies;
+//
+//	for (int i = 0; i < N; i++)
+//	{
+//		for (int j = 0; j < N; j++)
+//		{
+//			char Temp;
+//			cin >> Temp;
+//			Candies[i][j] = Temp;
+//		}
+//	}
+//
+//	for (int i = 0; i < N - 1; i++)
+//	{
+//		int Max = -1;
+//		for (int j = 0; j < N - 1; j++)
+//		{
+//			// 오른쪽이랑 비교
+//			int Temp = j + 1;
+//
+//			if (Candies[i][j] != Candies[i][Temp])
+//			{
+//				swap(Candies[i][j], Candies[i][Temp]);
+//
+//				int CurCandy = CheckMaxCandies(Candies, N);
+//				if (Max < CurCandy)
+//				{
+//					Max = CurCandy;
+//				}
+//			}
+//		}
+//	}
+//}
+
+#include <iostream>
 #include <vector>
-
 using namespace std;
+vector<vector<int>> board;
 
-int CheckMaxCandies(vector<vector<char>> C, int N)
+int checkBoard(int boardSize)
 {
-	for (int i = 0; i < N; i++)
+	// 가로 체크
+	int max = -1;
+	char prevWord;
+	for (int i = 0; i < boardSize; i++)
 	{
-		for (int j = 0; j < N; j++)
+		prevWord = board[i][0];
+		int count = 0;
+		for (int j = 0; j < boardSize; j++)
 		{
-
+			if (prevWord == board[i][j])
+				++count;
+			else
+			{
+				if (count > max) max = count;
+				count = 1;
+				prevWord = board[i][j];
+			}
 		}
+
+		if (count > max) max = count;
 	}
 
-	return 1;
+	for (int i = 0; i < boardSize; i++)
+	{
+		prevWord = board[i][0];
+		int count = 0;
+		for (int j = 0; j < boardSize; j++)
+		{
+			if (prevWord == board[j][i])
+				++count;
+			else
+			{
+				if (count > max) max = count;
+				count = 1;
+				prevWord = board[j][i];
+			}
+		}
+
+		if (count > max) max = count;
+	}
+
+	return max;
 }
 
 int main()
 {
-	int N;
-	cin >> N;
+	int boardSize;
+	cin >> boardSize;
 
-	vector<vector<char>> Candies;
+	board.resize(boardSize);
 
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < boardSize; i++)
 	{
-		for (int j = 0; j < N; j++)
+		board[i].resize(boardSize);
+	}
+
+	for (int i = 0; i < boardSize; i++)
+	{
+		for (int j = 0; j < boardSize; j++)
 		{
-			char Temp;
-			cin >> Temp;
-			Candies[i][j] = Temp;
+			char temp;
+			cin >> temp;
+			board[i][j] = temp;
 		}
 	}
 
-	for (int i = 0; i < N - 1; i++)
+	int maxCandy = -1;
+
+	for (int i = 0; i < boardSize; i++)
 	{
-		int Max = -1;
-		for (int j = 0; j < N - 1; j++)
+		for (int j = 0; j < boardSize; j++)
 		{
-			// 오른쪽이랑 비교
-			int Temp = j + 1;
-
-			if (Candies[i][j] != Candies[i][Temp])
+			if (j != boardSize - 1)
 			{
-				swap(Candies[i][j], Candies[i][Temp]);
-
-				int CurCandy = CheckMaxCandies(Candies, N);
-				if (Max < CurCandy)
+				if (board[i][j] != board[i][j + 1])
 				{
-					Max = CurCandy;
+					swap(board[i][j], board[i][j + 1]);
+					maxCandy = checkBoard(boardSize) > maxCandy ? checkBoard(boardSize) : maxCandy;
+					swap(board[i][j], board[i][j + 1]);
+				}
+			}
+
+			if (i != boardSize - 1)
+			{
+				if (board[i][j] != board[i + 1][j])
+				{
+					swap(board[i][j], board[i + 1][j]);
+					maxCandy = checkBoard(boardSize) > maxCandy ? checkBoard(boardSize) : maxCandy;
+					swap(board[i][j], board[i + 1][j]);
 				}
 			}
 		}
 	}
+
+	cout << maxCandy;
 }
