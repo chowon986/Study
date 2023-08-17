503a0d602b4c97046d748134dd86df4d35f4ca77
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-08-17 13:50:24 +0900

    김초원 : [브루트포스] 카잉 달력

---
+++ 백준 알고리즘/카잉 달력/카잉 달력.cpp
@@ -0,0 +1,97 @@
+﻿// 카잉 달력.cpp : https://www.acmicpc.net/problem/6064
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//
+//	int m;
+//	int n;
+//	int targetX;
+//	int targetY;
+//	int curX;
+//	int curY;
+//	int year;
+//
+//	int tc;
+//	cin >> tc;
+//
+//	for (int i = 0; i < tc; i++)
+//	{
+//		cin >> m;
+//		cin >> n;
+//		cin >> targetX;
+//		cin >> targetY;
+//		curX = 1;
+//		curY = 1;
+//		year = 1;
+//
+//		if (curX == m && curY == n)
+//		{
+//			cout << year << '\n';
+//			continue;
+//		}
+//
+//		while (curX != targetX || curY != targetY)
+//		{
+//			if (curX == m && curY == n)
+//			{
+//				year = -1;
+//				break;
+//			}
+//
+//			++curX;
+//			if (curX > m)
+//				curX = 1;
+//			++curY;
+//			if (curY > n)
+//				curY = 1;
+//			++year;
+//		}
+//
+//		cout << year << '\n';
+//	}
+//}
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int m;
+	int n;
+	int targetX;
+	int targetY;
+
+	int tc;
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++)
+	{
+		cin >> m;
+		cin >> n;
+		cin >> targetX;
+		targetX -= 1;
+		cin >> targetY;
+		targetY -= 1;
+		
+		bool ok = false;
+		for (int k = targetX; k < (m * n); k += m)
+		{
+			if (k % n == targetY)
+			{
+				cout << k + 1 << '\n';
+				ok = true;
+			}
+		}
+
+		if (!ok)
+			cout << -1 << '\n';
+	}
+}
