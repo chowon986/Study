1f1d11afb230ba35a99011fe698182a09f261254
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-08-17 20:03:35 +0900

    김초원 : [브루트포스] 1, 2, 3 더하기

---
+++ 백준 알고리즘/1, 2, 3 더하기/1, 2, 3 더하기.cpp
@@ -0,0 +1,29 @@
+﻿// 1, 2, 3 더하기.cpp : https://www.acmicpc.net/problem/9095
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int tc;
+	cin >> tc;
+
+	vector<int> g(12, 0);
+	g[0] = 0;
+	g[1] = 1;
+	g[2] = 2;
+	g[3] = 4;
+
+	for (int i = 4; i < 12; i++)
+	{
+		g[i] = g[i - 1] + g[i - 2] + g[i - 3];
+	}
+
+	for (int i = 0; i < tc; i++)
+	{
+		int temp;
+		cin >> temp;
+		cout << g[temp] << '\n';
+	}
+}
