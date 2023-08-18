f9da07d6654362f86085b4ad93550b96be5316bd
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-18 15:23:41 +0900

    김초원 : [브루트포스] N과 M (2)

---
+++ 백준 알고리즘/N과 M (2)/N과 M (2).cpp
@@ -0,0 +1,37 @@
+﻿// N과 M (2)
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<bool> c(10, false); // 앞에서 사용한 수인지?
+vector<int> a(10, false); // 수열을 저장
+
+void go(int index, int start, int n, int m)
+{
+	if (index == m)
+	{
+		for (int i = 0; i < index; i++)
+		{
+			cout << a[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = start; i <= n; i++)
+	{
+		if (c[i]) continue;
+		c[i] = true;
+		a[index] = i;
+		go(index + 1, i + 1, n, m);
+		c[i] = false;
+	}
+}
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	go(0, 1, a, b);
+}
