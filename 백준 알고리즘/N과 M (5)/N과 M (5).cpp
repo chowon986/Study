dd6130503c905276bb7264dd535c881ac382d01e
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-18 15:24:35 +0900

    김초원 : [브루트포스] N과 M (5)

---
+++ 백준 알고리즘/N과 M (5)/N과 M (5).cpp
@@ -0,0 +1,51 @@
+﻿// N과 M (5)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<bool> isUsed(10, false);
+vector<int> numbers;
+vector<int> newNumbers(10, 0);
+
+void go(int index, int count)
+{
+	if (index == count)
+	{
+		for (int i = 0; i < index; i++)
+		{
+			cout << newNumbers[i] << ' ';
+		}
+
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 0; i < numbers.size(); i++)
+	{
+		if (isUsed[i]) continue;
+		
+		isUsed[i] = true;
+
+		newNumbers[index] = numbers[i];
+		go(index + 1, count);
+		isUsed[i] = false;
+	}
+}
+
+int main()
+{
+	int tc, count;
+	cin >> tc >> count;
+
+	for (int i = 0; i < tc; i++)
+	{
+		int temp;
+		cin >> temp;
+		numbers.push_back(temp);
+	}
+
+	sort(numbers.begin(), numbers.end());
+	go(0, count);
+}
