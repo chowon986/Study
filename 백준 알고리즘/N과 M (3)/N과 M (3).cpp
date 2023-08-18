6e085369e0c2e43c6b4ecfce4605d41ae9b1b418
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-18 15:24:00 +0900

    김초원 : [브루트포스] N과 M (3)

---
+++ 백준 알고리즘/N과 M (3)/N과 M (3).cpp
@@ -0,0 +1,34 @@
+﻿// N과 M (3)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> numbers(10, 0);
+
+void go(int index, int maxNum, int count)
+{
+	if (index == count)
+	{
+		for (int i = 0; i < index; i++)
+		{
+			cout << numbers[i] << ' ';
+		}
+
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= maxNum; i++)
+	{
+		numbers[index] = i;
+		go(index + 1, maxNum, count);
+	}
+}
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	go(0, a, b);
+}
