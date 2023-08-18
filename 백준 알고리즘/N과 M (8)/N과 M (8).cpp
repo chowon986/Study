856dc4a854b10b1121c5153df58a8bbf4772fc3f
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-18 15:25:52 +0900

    김초원 : [브루트포스] N과 M (8)

---
+++ 백준 알고리즘/N과 M (8)/N과 M (8).cpp
@@ -0,0 +1,46 @@
+﻿// N과 M (8)
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<int> numbers;
+vector<int> newNumbers(10, 0);
+
+void go(int index, int start, int count)
+{
+	if (index == count)
+	{
+		for (int i = 0; i < index; i++)
+		{
+			cout << newNumbers[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = start; i < numbers.size(); i++)
+	{
+		newNumbers[index] = numbers[i];
+		go(index + 1, i, count);
+	}
+}
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+
+	for (int i = 0; i < a; i++)
+	{
+		int temp;
+		cin >> temp;
+
+		numbers.push_back(temp);
+	}
+
+	sort(numbers.begin(), numbers.end());
+
+	go(0, 0, b);
+}
