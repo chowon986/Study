9ad9f67cfc220115885a30e2c1ccebffd8825106
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-21 15:12:29 +0900

    김초원 : [브루트포스] 부등호

---
+++ 백준 알고리즘/부등호/부등호.cpp
@@ -0,0 +1,79 @@
+﻿// 부등호
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int tc;
+vector<char> inequalitySign;
+vector<bool> isUsed(10, false);
+string maxString = "";
+string minString = "";
+
+bool check(string& answer)
+{
+	for (int i = 0; i < tc; i++)
+	{
+		if (inequalitySign[i] == '<')
+		{
+			if (answer[i] > answer[i + 1])
+				return false;
+		}
+		else
+		{
+			if (answer[i] < answer[i + 1])
+				return false;
+		}
+	}
+
+	return true;
+}
+
+void go(int index, string answer)
+{
+	if (index == tc + 1)
+	{
+		if (check(answer))
+		{
+			if (maxString == "") maxString = answer;
+			if (minString == "") minString = answer;
+
+			if (maxString < answer) maxString = answer;
+			if (minString > answer) minString = answer;
+		}
+		return;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (isUsed[i]) continue;
+
+		isUsed[i] = true;
+		go(index + 1, answer + to_string(i));
+		isUsed[i] = false;
+	}
+}
+
+
+int main()
+{
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++)
+	{
+		char temp;
+		cin >> temp;
+		inequalitySign.push_back(temp);
+	}
+
+	go(0, "");
+
+	if (minString == maxString)
+		cout << minString;
+	else
+	{
+		cout << maxString << '\n';
+		cout << minString;
+	}
+}
