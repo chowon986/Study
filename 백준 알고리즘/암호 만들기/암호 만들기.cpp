cd695030cd7414ce9fcfec8a638273fcb005ddb5
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-21 12:42:55 +0900

    김초원 : [브루트포스] 퇴사

--- 백준 알고리즘/암호 만들기/암호 만들기.cpp
+++ 백준 알고리즘/암호 만들기/암호 만들기.cpp
@@ -1,50 +1,169 @@
 ﻿// 암호 만들기
 
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//vector<char> alpha;
+//
+//bool check(string password)
+//{
+//	int a = 0; int b = 0;
+//	for (int i = 0; i < password.size(); i++)
+//	{
+//		if (password[i] == 'a' || password[i] == 'e' || password[i] == 'i' || password[i] == 'o' || password[i] == 'u')
+//			++a;
+//
+//		else ++b;
+//	}
+//
+//	return (a >= 1 && b >= 2);
+//}
+//
+//void go(int n, vector<char>& alpha, string password, int i)
+//{
+//	if (password.length() == n)
+//	{
+//		if (check(password))
+//		{
+//			cout << password << '\n';
+//		}
+//		return;
+//	}
+//
+//	if (i >= alpha.size()) return;
+//	go(n, alpha, password + alpha[i], i + 1);
+//	go(n, alpha, password, i + 1);
+//}
+//
+//int main()
+//{
+//	// n 만들어야 하는 암호의 길이
+//	// alpha 사용할 수 있는 알파벳
+//	// password 현재까지 만든 암호
+//	// i 사용할지 말지 결정해야 하는 알파벳의 인덱스
+//	int n, m;
+//	cin >> n >> m;
+//
+//	for (int i = 0; i < m; i++)
+//	{
+//		char character;
+//		cin >> character;
+//		alpha.push_back(character);
+//	}
+//
+//	sort(alpha.begin(), alpha.end());
+//
+//	go(n, alpha, "", 0);
+//}
+
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//bool check(string password)
+//{
+//    int ja = 0;
+//    int mo = 0;
+//    for (int i = 0; i < password.length(); i++)
+//    {
+//        if (password[i] == 'a' || password[i] == 'e' || password[i] == 'i' || password[i] == 'o' || password[i] == 'u')
+//            ++mo;
+//        else
+//            ++ja;
+//    }
+//
+//    return (mo >= 1 && ja >= 2);
+//}
+//
+//void go(int n, vector<char>& alpha, string password, int i)
+//{
+//    if (password.length() == n)
+//    {
+//        if (check(password))
+//        {
+//            cout << password << '\n';
+//        }
+//        return;
+//    }
+//
+//    if (i >= alpha.size()) return;
+//    go(n, alpha, password + alpha[i], i + 1);
+//    go(n, alpha, password, i + 1);
+//}
+//
+//int main()
+//{
+//    vector<char> alpha;
+//    int n, m;
+//    cin >> n >> m;
+//
+//    for (int i = 0; i < m; i++)
+//    {
+//        char temp;
+//        cin >> temp;
+//        alpha.push_back(temp);
+//    }
+//
+//    sort(alpha.begin(), alpha.end());
+//
+//    go(n, alpha, "", 0);
+//}
+
 #include <iostream>
 #include <vector>
-#include <string>
 #include <algorithm>
 
 using namespace std;
 
-vector<char> alpha;
+vector<char> word;
+vector<char> newWord(26);
 
-bool check(string password)
+bool check(vector<char>& password, int index)
 {
-	int a = 0; int b = 0;
-	for (int i = 0; i < password.size(); i++)
+	int mo = 0;
+	int ja = 0;
+	for (int i = 0; i < index; i++)
 	{
 		if (password[i] == 'a' || password[i] == 'e' || password[i] == 'i' || password[i] == 'o' || password[i] == 'u')
-			++a;
-
-		else ++b;
+			++mo;
+		else
+			++ja;
 	}
 
-	return (a >= 1 && b >= 2);
+	return (mo >= 1 && ja >= 2);
 }
 
-void go(int n, vector<char>& alpha, string password, int i)
+void go(int index, int count, int start)
 {
-	if (password.length() == n)
+	if (index == count)
 	{
-		if (check(password))
+		if (check(newWord, index))
 		{
-			cout << password << '\n';
+			for (int i = 0; i < index; i++)
+			{
+				cout << newWord[i];
+			}
+
+			cout << '\n';
 		}
 		return;
 	}
 
-	if (i >= alpha.size()) return;
-	go(n, alpha, password + alpha[i], i + 1);
-	go(n, alpha, password, i + 1);
+	for (int i = start; i < word.size(); i++)
+	{
+		newWord[index] = word[i];
+		go(index + 1, count, i + 1);
+	}
 }
 
 int main()
 {
-	// n 만들어야 하는 암호의 길이
-	// alpha 사용할 수 있는 알파벳
-	// password 현재까지 만든 암호
-	// i 사용할지 말지 결정해야 하는 알파벳의 인덱스
 	int n, m;
 	cin >> n >> m;
 
@@ -52,10 +171,10 @@ int main()
 	{
 		char character;
 		cin >> character;
-		alpha.push_back(character);
+		word.push_back(character);
 	}
 
-	sort(alpha.begin(), alpha.end());
+	sort(word.begin(), word.end());
 
-	go(n, alpha, "", 0);
+	go(0, n, 0);
 }
---
+++ 백준 알고리즘/퇴사(재귀)/퇴사(재귀).cpp
@@ -0,0 +1,42 @@
+﻿// 퇴사(재귀)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> day;
+vector<int> money;
+
+int sum = 0;
+
+void go(int curDay, int curSum, int endDay, int i)
+{
+	if (curDay == endDay)
+	{
+		sum = max(sum, curSum);
+		return;
+	}
+
+	if (curDay > endDay) return;
+
+	go(curDay + day[i], curSum + money[i], endDay, i + day[i]);
+	go(curDay + 1, curSum, endDay, i + 1);
+}
+
+int main()
+{
+	int tc;
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++)
+	{
+		int n, m;
+		cin >> n >> m;
+		day.push_back(n);
+		money.push_back(m);
+	}
+
+	go(0, 0, tc, 0);
+
+	cout << sum;
+}
