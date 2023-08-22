f5a2f0daae6eb1d33e45464f2672148af6ee8f8b
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-22 11:03:00 +0900

    김초원 : [브루트포스] 외판원 순회 2

---
+++ 백준 알고리즘/외판원 순회 2/외판원 순회 2.cpp
@@ -0,0 +1,66 @@
+﻿// 외판원 순회
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> board;
+vector<int> numbers;
+int n;
+int sum = 2147483647;
+
+void check()
+{
+	int temp = 0;
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (board[numbers[i]][numbers[i + 1]] == 0)
+			return;
+		else
+			temp += board[numbers[i]][numbers[i + 1]];
+	}
+
+	if (board[numbers[n - 1]][numbers[0]] == 0)
+		return;
+
+	temp += board[numbers[n - 1]][numbers[0]];
+
+	sum = min(temp, sum);
+}
+
+int main()
+{
+	cin >> n;
+
+	board.resize(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		board[i].resize(n);
+
+		for (int j = 0; j < n; j++)
+		{
+			int temp;
+			cin >> temp;
+			board[i][j] = temp;
+		}
+	}
+
+
+	numbers.resize(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		numbers[i] = i;
+	}
+
+	do
+	{
+		check();
+	} while (next_permutation(numbers.begin(), numbers.end()));
+
+	cout << sum;
+}
