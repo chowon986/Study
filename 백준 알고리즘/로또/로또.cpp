86771a4c49728cf228d8eded427c1445dbcbe824
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-22 11:23:05 +0900

    김초원 : [브루트포스] 로또

---
+++ 백준 알고리즘/로또/로또.cpp
@@ -0,0 +1,48 @@
+﻿// 로또
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> newNumbers(50, 0);
+
+void go(vector<int>& numbers, int index, int count, int start)
+{
+	if (index == count)
+	{
+		for (int i = 0; i < index; i++)
+		{
+			cout << newNumbers[i] << ' ';
+		}
+		cout << '\n';
+	}
+
+	for (int i = start; i < numbers.size(); i++)
+	{
+		newNumbers[index] = numbers[i];
+		go(numbers, index + 1, count, i + 1);
+	}
+}
+
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		if (n == 0)
+			return 0;
+
+		vector<int> numbers(n);
+
+		for (int i = 0; i < n; i++)
+		{
+			int temp;
+			cin >> temp;
+
+			numbers[i] = temp;
+		}
+
+		go(numbers, 0, 6, 0);
+		cout << "\n";
+	}
+}
