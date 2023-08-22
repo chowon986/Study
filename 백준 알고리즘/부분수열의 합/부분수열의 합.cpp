2e0b41de2750e1726899cbd21dde5c180f8bd355
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-22 17:38:24 +0900

    김초원 : [브루트포스] 부분수열의 합

---
+++ 백준 알고리즘/부분수열의 합/부분수열의 합.cpp
@@ -0,0 +1,42 @@
+﻿// 부분수열의 합
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int ans = 0;
+	int n;
+	int s;
+	cin >> n >> s;
+
+	vector<int> a(n, 0);
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		a[i] = temp;
+	}
+
+	for (int i = 1; i < (1 << n); i++)
+	{
+		int sum = 0;
+
+		for (int k = 0; k < n; k++)
+		{
+			if (i & (1 << k)) // 비트 마스크 i에 k가 들어있는지 확인
+			{
+				sum += a[k];
+			}
+		}
+
+		if (sum == s)
+		{
+			ans += 1;
+		}
+	}
+
+	cout << ans;
+}
