049df011091a65136d423f2a3523ed8d9d8d89e0
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-22 10:12:44 +0900

    김초원 : [브루트포스] 다음 순열

---
+++ 백준 알고리즘/다음 순열/다음 순열.cpp
@@ -0,0 +1,33 @@
+﻿// 다음 순열
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector<int> numbers;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		numbers.push_back(temp);
+	}
+
+	if (next_permutation(numbers.begin(), numbers.end()))
+	{
+		for (int i = 0; i < n; i++)
+		{
+			cout << numbers[i] << ' ';
+		}
+	}
+	else
+		cout << "-1";
+
+	return 0;
+}
