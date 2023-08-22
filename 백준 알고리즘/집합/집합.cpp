5b29cba6ca23bd854894c60f5e2c11ac02f76cce
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-22 17:24:27 +0900

    김초원 : [브루트포스] 집합

---
+++ 백준 알고리즘/집합/집합.cpp
@@ -0,0 +1,53 @@
+﻿// 집합
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int ans = 0;
+
+	int tc;
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++)
+	{
+		string word;
+		int value;
+		cin >> word;
+		if (word != "all" && word != "empty")
+			cin >> value;
+
+		if (word == "add")
+		{
+			ans |= (1 << value);
+		}
+		else if (word == "remove")
+		{
+			ans &= ~(1 << value);
+		}
+		else if (word == "check")
+		{
+			if (ans & (1 << value))
+				cout << 1 << '\n';
+			else
+				cout << 0 << '\n';
+		}
+		else if (word == "toggle")
+		{
+			ans ^= (1 << value);
+		}
+		else if (word == "all")
+		{
+			ans = (1 << 21) - 1;
+		}
+		else if (word == "empty")
+		{
+			ans = 0;
+		}
+	}
+}
