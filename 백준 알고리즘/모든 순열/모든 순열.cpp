42af598b119dc32e55cd6e80dd7d8418dd178aff
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-22 10:21:03 +0900

    김초원 : 모든 순열

---
+++ 백준 알고리즘/모든 순열/모든 순열.cpp
@@ -0,0 +1,30 @@
+﻿// 모든 순열
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> numbers;
+
+	for (int i = 1; i <= n; i++)
+	{
+		numbers.push_back(i);
+	}
+
+	do
+	{
+		for (int i = 0; i < n; i++)
+		{
+			cout << numbers[i] << ' ';
+		}
+
+		cout << '\n';
+	} while (next_permutation(numbers.begin(), numbers.end()));
+
+}
