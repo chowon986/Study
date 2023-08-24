b4a368ce1aceda7a114beda915725ff58fc4c07d
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-24 16:12:38 +0900

    김초원 : [DP] 1, 2, 3 더하기 5

---
+++ 백준 알고리즘/1, 2, 3 더하기 5/1, 2, 3 더하기 5.cpp
@@ -0,0 +1,34 @@
+﻿// 1, 2, 3 더하기 : https://www.acmicpc.net/problem/15990
+
+#include <iostream>
+using namespace std;
+
+#define MAX 100000
+#define MOD 1000000009
+
+long long D[MAX + 1][4];
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int tc;
+	cin >> tc;
+
+	D[1][1] = D[2][2] = D[3][1] = D[3][2] = D[3][3] = 1;
+
+	for (int i = 4; i <= MAX; i++)
+	{
+		D[i][1] = ((D[i - 1][2] % MOD) + (D[i - 1][3] % MOD)) % MOD;
+		D[i][2] = ((D[i - 2][1] % MOD) + (D[i - 2][3] % MOD)) % MOD;
+		D[i][3] = ((D[i - 3][1] % MOD) + (D[i - 3][2] % MOD)) % MOD;
+	}
+
+	for (int i = 0; i < tc; i++)
+	{
+		int temp;
+		cin >> temp;
+		cout << (D[temp][1] + D[temp][2] + D[temp][3]) % MOD << '\n';
+	}
+}
