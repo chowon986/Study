053dcb47e08d0ab9e5b4c5216f92e34ea0329aa5
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-24 14:40:09 +0900

    김초원 : [DP] 2xn 타일링

---
+++ 백준 알고리즘/2xN 타일링/2xN 타일링.cpp
@@ -0,0 +1,23 @@
+﻿// 2xN 타일링 : https://www.acmicpc.net/problem/11726
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> f(1000);
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	f[0] = 1;
+	f[1] = 1;
+
+	for (int i = 2; i <= n; i++)
+	{
+		f[i] = f[i - 1] + f[i - 2];
+	}
+
+	cout << f[n];
+}
