c2ec181a9d8fccc41f4584f1d0443e5e305910d5
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-24 16:37:19 +0900

    김초원 : [DP] 쉬운 계단 수

---
+++ 백준 알고리즘/쉬운 계단 수/쉬운 계단 수.cpp
@@ -0,0 +1,49 @@
+﻿// 쉬운 계단 수 : https://www.acmicpc.net/problem/10844
+
+#include <iostream>
+using namespace std;
+
+#define MOD 1000000000
+
+int main()
+{
+	int n;
+	cin >> n;
+	int d[101][10] = { 0, };
+
+	for (int i = 1; i <= 9; i++)
+	{
+		d[1][i] = 1;
+	}
+
+	for (int i = 2; i <= n; i++)
+	{
+		for (int j = 0; j <= 9; j++)
+		{
+
+			if (d[i][j] > 0)
+			{
+				continue;
+			}
+
+			if (j == 0)
+				d[i][j] = d[i - 1][j + 1] % MOD;
+
+			else if (j == 9)
+				d[i][j] = d[i - 1][j - 1] % MOD;
+
+			else
+			{
+				d[i][j] = (d[i - 1][j - 1] + d[i - 1][j + 1]) % MOD;
+			}
+		}
+	}
+
+	long long sum = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		sum += d[n][i];
+	}
+
+	cout << sum % MOD;
+}
