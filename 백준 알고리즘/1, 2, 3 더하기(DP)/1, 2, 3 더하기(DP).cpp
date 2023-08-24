57779c2e36b911a978e88fb42b6cd215c448d37c
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-24 14:58:19 +0900

    김초원 : [DP] 1, 2, 3 더하기

---
+++ 백준 알고리즘/1, 2, 3 더하기(DP)/1, 2, 3 더하기(DP).cpp
@@ -0,0 +1,30 @@
+﻿// 1, 2, 3 더하기(DP) : https://www.acmicpc.net/problem/9095
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> d(12, 0);
+
+int main()
+{
+	int tc;
+	cin >> tc;
+
+	d[0] = 1;
+	d[1] = 1;
+	d[2] = 2;
+
+	for (int i = 3; i <= 11; i++)
+	{
+		d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+	}
+
+
+	for (int i = 0; i < tc; i++)
+	{
+		int temp;
+		cin >> temp;
+		cout << d[temp] << '\n';
+	}
+}
