2022c8d33882f19a7694ec94d8781044b25c8f81
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-24 15:37:18 +0900

    김초원 : [DP] 카드 구매하기

---
+++ 백준 알고리즘/카드 구매하기/카드 구매하기.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> p(1001);
+vector<int> d(1001);
+
+int main()
+{
+	int tc;
+	cin >> tc;
+
+	for (int i = 1; i <= tc; i++)
+	{
+		int temp;
+		cin >> temp;
+		p[i] = temp;
+	}
+
+	for (int i = 1; i <= tc; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			d[i] = max(d[i], d[i - j] + p[j]);
+		}
+	}
+
+	cout << d[tc];
+}
