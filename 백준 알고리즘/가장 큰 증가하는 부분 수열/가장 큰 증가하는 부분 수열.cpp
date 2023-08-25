8f836ee47dfd47e386ad7b2447ce0889e3671fed
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-25 14:30:49 +0900

    김초원 : [DP] 가장 큰 증가하는 부분 수열

--- 백준 알고리즘/가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cpp
+++ 백준 알고리즘/가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cpp
@@ -1,48 +1,85 @@
-﻿/*
-점화식
-D[i] = i번까지 있을때 최대 값
-D[i] = A[1];
-*/
+﻿///*
+//점화식
+//D[i] = i번까지 있을때 최대 값
+//D[i] = A[1];
+//*/
+//
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//vector<int> A(1001, 1);
+//vector<int> D(1001, 0);
+//
+//int main()
+//{
+//	int Cnt;
+//	cin >> Cnt;
+//
+//	for (int i = 1; i <= Cnt; i++)
+//	{
+//		int N;
+//		cin >> N;
+//		A[i] = N;
+//		D[i] = N;
+//	}
+//
+//	for (int i = 1; i <= Cnt; i++)
+//	{
+//		for (int j = i; j > 0; j--)
+//		{
+//			if (A[i] > A[j])
+//			{
+//				D[i] = max(D[i], D[j] + A[i]);
+//			}
+//		}
+//	}
+//
+//	int Max = -1;
+//	for (int i = 1; i <= Cnt; i++)
+//	{
+//		if (Max < D[i])
+//		{
+//			Max = D[i];
+//		}
+//	}
+//
+//	cout << Max;
+//}
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
-vector<int> A(1001, 1);
-vector<int> D(1001, 0);
-
 int main()
 {
-	int Cnt;
-	cin >> Cnt;
+	int n;
+	cin >> n;
 
-	for (int i = 1; i <= Cnt; i++)
+	vector<int> numbers;
+	vector<int> d(n + 1, 1);
+
+	int maxValue = -1;
+
+	for (int i = 0; i < n; i++)
 	{
-		int N;
-		cin >> N;
-		A[i] = N;
-		D[i] = N;
+		int temp;
+		cin >> temp;
+		numbers.push_back(temp);
 	}
 
-	for (int i = 1; i <= Cnt; i++)
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = i; j > 0; j--)
+		for (int j = i; j >= 0; j--)
 		{
-			if (A[i] > A[j])
+			if (numbers[i] > numbers[j])
 			{
-				D[i] = max(D[i], D[j] + A[i]);
+				d[i] = max(d[i], d[j] + 1);
 			}
 		}
+
+		maxValue = max(maxValue, d[i]);
 	}
 
-	int Max = -1;
-	for (int i = 1; i <= Cnt; i++)
-	{
-		if (Max < D[i])
-		{
-			Max = D[i];
-		}
-	}
-
-	cout << Max;
+	cout << maxValue;
 }
