ac38654225ff582ccbb63fadbbc060a5d70062e8
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-25 15:31:40 +0900

    김초원 : [DP] 제곱수의 합

--- 백준 알고리즘/제곱수의 합/제곱수의 합.cpp
+++ 백준 알고리즘/제곱수의 합/제곱수의 합.cpp
@@ -1,22 +1,44 @@
-﻿#include <iostream>
+﻿//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main()
+//{
+//	int N = 0;
+//	cin >> N;
+//	vector<int> D(N + 1, 0);
+//
+//	for (int i = 1; i <= N; i++)
+//	{
+//		D[i] = i; // 모든 수를 1로 채운 경우 -> Max 케이스
+//		for (int j = 1; j * j <= i; j++)
+//		{
+//			D[i] = min(D[i], D[i - j * j] + 1);
+//		}
+//	}
+//
+//	cout << D[N];
+//}
+
+#include <iostream>
 #include <vector>
 using namespace std;
 
-
 int main()
 {
-	int N = 0;
-	cin >> N;
-	vector<int> D(N + 1, 0);
+	int n = 0;
+	cin >> n;
 
-	for (int i = 1; i <= N; i++)
+	vector<int> d(n + 1, 0);
+
+	for (int i = 1; i <= n; i++)
 	{
-		D[i] = i; // 모든 수를 1로 채운 경우 -> Max 케이스
+		d[i] = i;
 		for (int j = 1; j * j <= i; j++)
 		{
-			D[i] = min(D[i], D[i - j * j] + 1);
+			d[i] = min(d[i], d[i - j * j] + 1);
 		}
 	}
 
-	cout << D[N];
+	cout << d[n];
 }
