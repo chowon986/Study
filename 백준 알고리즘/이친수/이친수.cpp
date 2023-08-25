001181148bb15933238de4e75d8e917d6946d06d
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-25 13:44:55 +0900

    김초원 : [DP] 이친수

---
+++ 백준 알고리즘/이친수/이친수.cpp
@@ -0,0 +1,25 @@
+﻿// 이친수
+
+#include <iostream>
+using namespace std;
+
+long long D[91][2];
+// D[i][0] = D[i-1][0] + D[i-1][1]
+// D[i][1] = D[i-1][0]
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	D[1][0] = 0;
+	D[1][1] = 1;
+
+	for (int i = 2; i <= n; i++)
+	{
+		D[i][0] += D[i - 1][0] + D[i - 1][1];
+		D[i][1] += D[i - 1][0];
+	}
+
+	cout << D[n][0] + D[n][1];
+}
