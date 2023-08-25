62a89bd199a9880927f0cc9c0e50aa475f8d3388
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-08-25 14:52:43 +0900

    김초원 : [DP] 가장 큰 증가하는 부분 수열 4

--- 백준 알고리즘/가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cpp
+++ 백준 알고리즘/가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cpp
@@ -1,69 +1,129 @@
-﻿#include <iostream>
+﻿//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int A[1001];
+//int D[1001];
+//int V[1001];
+//
+//int main()
+//{
+//	int Cnt;
+//	cin >> Cnt;
+//
+//	for (int i = 1; i <= Cnt; i++)
+//	{
+//		cin >> A[i];
+//		D[i] = 1;
+//	}
+//
+//	if (Cnt == 1)
+//	{
+//		cout << 1 << '\n';
+//		cout << A[1];
+//		return 0;
+//	}
+//
+//	int Max = 0;
+//	int MaxIndex = 0;
+//
+//	for (int i = 2; i <= Cnt; i++)
+//	{
+//		for (int j = i - 1; j > 0; j--)
+//		{
+//			if (A[i] > A[j])
+//			{
+//				if (D[i] < D[j] + 1)
+//				{
+//					D[i] = D[j] + 1;
+//					V[i] = j;
+//				}
+//			}
+//		}
+//
+//		if (D[i] > Max)
+//		{
+//			Max = D[i];
+//			MaxIndex = i;
+//		}
+//	}
+//
+//	cout << Max << '\n';
+//
+//	vector<int> Numbers;
+//	while (true)
+//	{
+//		Numbers.push_back(A[MaxIndex]);
+//		MaxIndex = V[MaxIndex];
+//		if (MaxIndex == 0)
+//		{
+//			while (Numbers.size() != 0)
+//			{
+//				cout << Numbers.back() << ' ';
+//				Numbers.pop_back();
+//			}
+//
+//			return 0;
+//		}
+//	}
+//}
+
+#include <iostream>
+#include <stack>
 #include <vector>
 using namespace std;
 
-int A[1001];
-int D[1001];
-int V[1001];
+vector<int> numbers;
+vector<int> index(1001, -1);
+
+void go(int maxIndex)
+{
+	if (maxIndex == -1) return;
+
+	go(index[maxIndex]);
+
+	cout << numbers[maxIndex] << ' ';
+}
 
 int main()
 {
-	int Cnt;
-	cin >> Cnt;
+	int n;
+	cin >> n;
 
-	for (int i = 1; i <= Cnt; i++)
+	int maxValue = -1;
+	int maxIndex = -1;
+
+	vector<int> count(1001, 1);
+
+	for (int i = 0; i < n; i++)
 	{
-		cin >> A[i];
-		D[i] = 1;
+		int temp;
+		cin >> temp;
+		numbers.push_back(temp);
 	}
 
-	if (Cnt == 1)
+	for (int i = 0; i < n; i++)
 	{
-		cout << 1 << '\n';
-		cout << A[1];
-		return 0;
-	}
-
-	int Max = 0;
-	int MaxIndex = 0;
-
-	for (int i = 2; i <= Cnt; i++)
-	{
-		for (int j = i - 1; j > 0; j--)
+		for (int j = i; j >= 0; j--)
 		{
-			if (A[i] > A[j])
+			if (numbers[i] > numbers[j])
 			{
-				if (D[i] < D[j] + 1)
+				if (count[i] < (count[j] + 1))
 				{
-					D[i] = D[j] + 1;
-					V[i] = j;
+					count[i] = count[j] + 1;
+					index[i] = j;
 				}
 			}
 		}
 
-		if (D[i] > Max)
+		if (maxValue < count[i])
 		{
-			Max = D[i];
-			MaxIndex = i;
+			maxValue = count[i];
+			maxIndex = i;
 		}
 	}
 
-	cout << Max << '\n';
+	cout << maxValue << '\n';
 
-	vector<int> Numbers;
-	while (true)
-	{
-		Numbers.push_back(A[MaxIndex]);
-		MaxIndex = V[MaxIndex];
-		if (MaxIndex == 0)
-		{
-			while (Numbers.size() != 0)
-			{
-				cout << Numbers.back() << ' ';
-				Numbers.pop_back();
-			}
-
-			return 0;
-		}
-	}
+	go(maxIndex);
 }
