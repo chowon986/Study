565536316cce53716c6e85585e5813d183c20c96
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-08-28 15:33:46 +0900

    김초원 : [DP] 동물원

--- 백준 알고리즘/동물원/동물원.cpp
+++ 백준 알고리즘/동물원/동물원.cpp
@@ -1,25 +1,51 @@
-﻿#include <iostream>
-#include <vector>
+﻿// 이전 풀이
 
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int D[100001][3];
+//
+//int main()
+//{
+//	int N;
+//	cin >> N;
+//
+//	D[0][0] = 1;
+//	D[0][1] = 0;
+//	D[0][2] = 0;
+//
+//	for (int i = 1; i <= N; i++)
+//	{
+//		D[i][0] = (D[i - 1][0] + D[i - 1][1] + D[i - 1][2]) % 9901;
+//		D[i][1] = (D[i - 1][0] + D[i - 1][2]) % 9901;
+//		D[i][2] = (D[i - 1][0] + D[i - 1][1]) % 9901;
+//	}
+//
+//	cout << (D[N][0] + D[N][1] + D[N][2]) % 9901;
+//}
+
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int D[100001][3];
+int d[100001][3];
 
 int main()
 {
-	int N;
-	cin >> N;
+	int n;
+	cin >> n;
 
-	D[0][0] = 1;
-	D[0][1] = 0;
-	D[0][2] = 0;
+	d[0][0] = 1;
+	d[0][1] = d[0][2] = 0;
 
-	for (int i = 1; i <= N; i++)
+	for (int i = 1; i <= n; i++)
 	{
-		D[i][0] = (D[i - 1][0] + D[i - 1][1] + D[i - 1][2]) % 9901;
-		D[i][1] = (D[i - 1][0] + D[i - 1][2]) % 9901;
-		D[i][2] = (D[i - 1][0] + D[i - 1][1]) % 9901;
+		d[i][0] = (d[i - 1][0] + d[i - 1][1] + d[i - 1][2]) % 9901;
+		d[i][1] = (d[i - 1][0] + d[i - 1][2]) % 9901;
+		d[i][2] = (d[i - 1][0] + d[i - 1][1]) % 9901;
 	}
 
-	cout << (D[N][0] + D[N][1] + D[N][2]) % 9901;
+	cout << (d[n][0] + d[n][1] + d[n][2]) % 9901;
 }
