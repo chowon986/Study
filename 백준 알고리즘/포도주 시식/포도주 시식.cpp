9767f5e3c102358e4058cd3f6000b0a81c91ef7b
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-01 15:04:27 +0900

    김초원 : [DP] 포도주 시식

--- 백준 알고리즘/포도주 시식/포도주 시식.cpp
+++ 백준 알고리즘/포도주 시식/포도주 시식.cpp
@@ -1,35 +1,69 @@
-﻿#include <iostream>
+﻿// 이전 풀이
+
+//#include <iostream>
+//using namespace std;
+//
+//int D[10001][4];
+//int A[10001];
+//
+//int main()
+//{
+//	int Cnt;
+//	cin >> Cnt;
+//
+//	for (int i = 1; i <= Cnt; i++)
+//	{
+//		int N;
+//		cin >> N;
+//		A[i] = N;
+//	}
+//
+//	for (int i = 1; i <= Cnt; i++)
+//	{
+//		D[i][0] = max(D[i - 1][0], max(D[i - 1][1], D[i - 1][2]));
+//		D[i][1] = D[i - 1][0] + A[i];
+//		D[i][2] = D[i - 1][1] + A[i];
+//	}
+//
+//	int Max = -1;
+//
+//	for (int i = 0; i < 3; i++)
+//	{
+//		if (Max < D[Cnt][i])
+//			Max = D[Cnt][i];
+//	}
+//
+//	cout << Max;
+//}
+
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int D[10001][4];
-int A[10001];
+int a[10001];
 
 int main()
 {
-	int Cnt;
-	cin >> Cnt;
+	int n;
+	cin >> n;
 
-	for (int i = 1; i <= Cnt; i++)
+	vector<vector<int>> d(10001);
+
+	d[0].resize(3);
+	for (int i = 1; i <= n; i++)
 	{
-		int N;
-		cin >> N;
-		A[i] = N;
+		d[i].resize(3);
+		int temp;
+		cin >> temp;
+		a[i] = temp;
 	}
 
-	for (int i = 1; i <= Cnt; i++)
+	for (int i = 1; i <= n; i++)
 	{
-		D[i][0] = max(D[i - 1][0], max(D[i - 1][1], D[i - 1][2]));
-		D[i][1] = D[i - 1][0] + A[i];
-		D[i][2] = D[i - 1][1] + A[i];
+		d[i][0] = max(d[i - 1][0], max(d[i - 1][1], d[i - 1][2]));
+		d[i][1] = d[i - 1][0] + a[i];
+		d[i][2] = d[i - 1][1] + a[i];
 	}
 
-	int Max = -1;
-
-	for (int i = 0; i < 3; i++)
-	{
-		if (Max < D[Cnt][i])
-			Max = D[Cnt][i];
-	}
-
-	cout << Max;
+	cout << max(d[n][0], max(d[n][1], d[n][2]));
 }
