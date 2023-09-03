c6fd6e7bd3f98815b0f4ac504555f9422ab6a3dc
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-04 02:00:36 +0900

    김초원 : [구현] 상하좌우

---
+++ 이것이 취업을 위한 코딩테스트다/00 상하좌우/00 상하좌우.cpp
@@ -0,0 +1,43 @@
+﻿// 00 상하좌우
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+char a[101];
+int xPos[4] = { -1, 1, 0, 0 };
+int yPos[4] = { 0, 0, -1, 1 }; 
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int curX = 1;
+	int curY = 1;
+
+	cin.ignore();
+	string temp;
+	getline(cin, temp);
+
+	for(int i = 0; i < temp.size(); i++)
+	{
+		int value = -1;
+		if (temp[i] == 'U') value = 0;
+		if (temp[i] == 'D') value = 1;
+		if (temp[i] == 'L') value = 2;
+		if (temp[i] == 'R') value = 3;
+
+		if (value != -1)
+		{
+			if ((curX + xPos[value] > 0 && curY + yPos[value] > 0) &&
+				(curX + xPos[value] <= n && curY + yPos[value] <= n))
+			{
+				curX += xPos[value];
+				curY += yPos[value];
+			}
+		}
+	}
+
+	cout << curX << ' ' << curY;
+}
