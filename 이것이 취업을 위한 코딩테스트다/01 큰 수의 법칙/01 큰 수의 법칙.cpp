34d2d87fd2a21c05a0f5a2852a7d63c844ec11b2
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-04 01:59:54 +0900

    김초원 : [그리디] 큰 수의 법칙

---
+++ 이것이 취업을 위한 코딩테스트다/01 큰 수의 법칙/01 큰 수의 법칙.cpp
@@ -0,0 +1,37 @@
+﻿// 01 큰 수의 법칙
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n, m, k;
+	cin >> n >> m >> k;
+
+	vector<int> a(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		a[i] = temp;
+	}
+
+	sort(a.begin(), a.end(), greater<>());
+
+	int count = 0;
+	int ans = 0;
+	int num1 = a[0];
+	int num2 = a[1];
+
+	count = m / (k + 1) * k; // num1의 개수
+	count += m % (k + 1); // 나누어 떨어지지 않을 때 추가되는 num1의 개수
+
+	ans += count * num1; // count만큼 num1을 곱해서 더해주고
+	ans += (m - count) * num2; // 전체에서 count를 뺀 만큼 num2를 곱해서 더해주면 됨
+
+	cout << ans;
+}
