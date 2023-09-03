5966c86ce459037cf5f0db7234bb54710635af38
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-03 23:19:48 +0900

    김초원 : [그리디] 거스름돈

---
+++ 이것이 취업을 위한 코딩테스트다/00 거스름돈/00 거스름돈.cpp
@@ -0,0 +1,23 @@
+﻿// 00 거스름돈
+
+#include <iostream>
+using namespace std;
+
+int change[4] = { 500, 100, 50, 10 };
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int ans = 0;
+	int arrayLength = sizeof(change) / sizeof(int);
+
+	for (int i = 0; i < arrayLength; i++)
+	{
+		ans += n / change[i];
+		n %= change[i];
+	}
+
+	cout << ans;
+}
