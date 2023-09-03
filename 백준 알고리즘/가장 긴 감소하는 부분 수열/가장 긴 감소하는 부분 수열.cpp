afcc966cb08ce32cecbfdc7100135b0599f31746
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-09-03 15:00:04 +0900

    김초원 : [DP] 가장 긴 감소하는 부분 수열

--- 백준 알고리즘/가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cpp
+++ 백준 알고리즘/가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cpp
@@ -1,43 +1,89 @@
-﻿#include <iostream>
+﻿// 이전 풀이
+
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main()
+//{
+//	int Cnt;
+//	cin >> Cnt;
+//
+//	vector<int> D(Cnt + 1, 0);
+//	vector<int> A(Cnt + 1, 0);
+//
+//	for (int i = Cnt; i > 0; i--)
+//	{
+//		int N;
+//		cin >> N;
+//		D[i] = N;
+//		A[i] = 1;
+//	}
+//
+//
+//	for (int i = 1; i <= Cnt; i++)
+//	{
+//		for (int j = i - 1; j > 0; j--)
+//		{
+//			if (D[i] > D[j])
+//			{
+//				A[i] = max(A[i], A[j] + 1);
+//			}
+//		}
+//	}
+//
+//	int Max = -1;
+//	for (int i = 1; i <= Cnt; i++)
+//	{
+//		if (Max < A[i])
+//		{
+//			Max = A[i];
+//		}
+//	}
+//
+//	cout << Max;
+//}
+
+#include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-	int Cnt;
-	cin >> Cnt;
+	int n;
+	cin >> n;
 
-	vector<int> D(Cnt + 1, 0);
-	vector<int> A(Cnt + 1, 0);
+	vector<int> a;
 
-	for (int i = Cnt; i > 0; i--)
+	for (int i = 0; i < n; i++)
 	{
-		int N;
-		cin >> N;
-		D[i] = N;
-		A[i] = 1;
+		int temp;
+		cin >> temp;
+		a.push_back(temp);
 	}
 
+	reverse(a.begin(), a.end());
 
-	for (int i = 1; i <= Cnt; i++)
+	vector<int> d(n, 1);
+
+	for (int i = 1; i < n; i++)
 	{
-		for (int j = i - 1; j > 0; j--)
+		for (int j = i - 1; j >= 0; j--)
 		{
-			if (D[i] > D[j])
+			if (a[i] > a[j])
 			{
-				A[i] = max(A[i], A[j] + 1);
+				d[i] = max(d[i], d[j] + 1);
 			}
 		}
 	}
 
-	int Max = -1;
-	for (int i = 1; i <= Cnt; i++)
+	int maxValue = -1;
+
+	for (int i = 0; i < n; i++)
 	{
-		if (Max < A[i])
-		{
-			Max = A[i];
-		}
+		maxValue = max(maxValue, d[i]);
 	}
 
-	cout << Max;
+	cout << maxValue;
 }
