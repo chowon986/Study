2090dd4e3a8f25d6866271c8c3ff20a9d8145665
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-04 02:00:11 +0900

    김초원 : [그리디] 숫자 카드 게임

---
+++ 이것이 취업을 위한 코딩테스트다/02 숫자 카드 게임/02 숫자 카드 게임.cpp
@@ -0,0 +1,71 @@
+﻿// 02 숫자 카드 게임
+
+// 내 풀이
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int a[101][101];
+//
+//int main()
+//{
+//	int n, m;
+//	cin >> n >> m;
+//
+//	int ans = -1;
+//	for (int i = 0; i < n; i++)
+//	{
+//		int minValue = 10001;
+//		for (int j = 0; j < m; j++)
+//		{
+//			int temp;
+//			cin >> temp;
+//
+//			a[i][j] = temp;
+//
+//			minValue = min(temp, minValue);
+//		}
+//
+//		if (ans < minValue)
+//			ans = minValue;
+//	}
+//
+//	cout << ans;
+//}
+
+// 책 풀이
+
+#include <iostream>
+#include <algorithm>
+#include <list>
+
+using namespace std;
+
+int a[101][101];
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+
+	int minValue;
+	int result = -1;
+
+	for (int i = 0; i < n; i++)
+	{
+		list<int> data;
+		for (int j = 0; j < m; j++)
+		{
+			int temp;
+			cin >> temp;
+
+			data.push_back(temp);
+		}
+
+		minValue = *min_element(data.begin(), data.end());
+		result = max(result, minValue);
+	}
+
+	cout << result;
+}
