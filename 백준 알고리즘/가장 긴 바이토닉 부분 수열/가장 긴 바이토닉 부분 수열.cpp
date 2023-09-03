9dc1f67bd95ca6f11ca07318eac9d0292e77d8a5
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-09-03 15:18:53 +0900

    김초원 : [DP] 가장 긴 바이토닉 부분 수열

--- 백준 알고리즘/가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cpp
+++ 백준 알고리즘/가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cpp
@@ -1,55 +1,119 @@
-﻿#include <iostream>
+﻿// 이전 풀이
+
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main()
+//{
+//	int Cnt;
+//	cin >> Cnt;
+//
+//	vector<int> Numbers(Cnt + 1, 0);
+//	vector<int> U(Cnt + 1, 0);
+//	vector<int> D(Cnt + 1, 0);
+//
+//	for (int i = 1; i <= Cnt; i++)
+//	{
+//		int N;
+//		cin >> N;
+//		Numbers[i] = N;
+//		U[i] = 1;
+//		D[i] = 1;
+//	}
+//
+//	for (int i = 1; i <= Cnt; i++)
+//	{
+//		for (int j = i - 1; j > 0; j--)
+//		{
+//			if (Numbers[i] > Numbers[j])
+//			{
+//				U[i] = max(U[i], U[j] + 1);
+//			}
+//		}
+//	}
+//
+//	for (int i = Cnt - 1; i > 0; i--)
+//	{
+//		for (int j = i + 1; j <= Cnt; j++)
+//		{
+//			if (Numbers[i] > Numbers[j])
+//			{
+//				D[i] = max(D[i], D[j] + 1);
+//			}
+//		}
+//	}
+//
+//	int Max = -1;
+//	for (int i = 1; i <= Cnt; i++)
+//	{
+//		if (Max < U[i] + D[i])
+//		{
+//			Max = U[i] + D[i];
+//		}
+//	}
+//
+//	cout << Max - 1;
+//}
+
+#include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-	int Cnt;
-	cin >> Cnt;
+	vector<int> a;
 
-	vector<int> Numbers(Cnt + 1, 0);
-	vector<int> U(Cnt + 1, 0);
-	vector<int> D(Cnt + 1, 0);
+	int n;
+	cin >> n;
 
-	for (int i = 1; i <= Cnt; i++)
+	for (int i = 0; i < n; i++)
 	{
-		int N;
-		cin >> N;
-		Numbers[i] = N;
-		U[i] = 1;
-		D[i] = 1;
+		int temp;
+		cin >> temp;
+		a.push_back(temp);
 	}
 
-	for (int i = 1; i <= Cnt; i++)
+	int num;
+
+	vector<int> dec(n, 1);
+	vector<int> inc(n, 1);
+
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = i - 1; j > 0; j--)
+		num = a[i];
+
+		for (int j = i - 1; j >= 0; j--)
 		{
-			if (Numbers[i] > Numbers[j])
+			if (a[i] > a[j])
 			{
-				U[i] = max(U[i], U[j] + 1);
+				inc[i] = max(inc[i], inc[j] + 1);
 			}
 		}
 	}
 
-	for (int i = Cnt - 1; i > 0; i--)
+	reverse(a.begin(), a.end());
+
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = i + 1; j <= Cnt; j++)
+		for (int j = i - 1; j >= 0; j--)
 		{
-			if (Numbers[i] > Numbers[j])
+			if (a[i] > a[j])
 			{
-				D[i] = max(D[i], D[j] + 1);
+				dec[i] = max(dec[i], dec[j] + 1);
 			}
 		}
 	}
 
-	int Max = -1;
-	for (int i = 1; i <= Cnt; i++)
+	reverse(dec.begin(), dec.end());
+
+	int maxValue = -1;
+
+	for (int i = 0; i < n; i++)
 	{
-		if (Max < U[i] + D[i])
-		{
-			Max = U[i] + D[i];
-		}
+		maxValue = max(maxValue, inc[i] + dec[n - i - 1]);
 	}
 
-	cout << Max - 1;
+	cout << maxValue - 1;
 }
