77f39a67391684f98d11077582e3f923a13d7869
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-04 02:00:24 +0900

    김초원 : [그리디] 1이 될 때까지

---
+++ 이것이 취업을 위한 코딩테스트다/03 1이 될 때까지/03 1이 될 때까지.cpp
@@ -0,0 +1,24 @@
+﻿// 03 1이 될 때까지
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+
+	int count = 0;
+
+	while (n != 1)
+	{
+		++count;
+		if (n % k == 0)
+			n /= k;
+		else
+			n -= 1;
+	}
+
+	cout << count;
+}
