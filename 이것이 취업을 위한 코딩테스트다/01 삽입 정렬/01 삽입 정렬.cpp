fd869e0f93fafd53934885a4bbb862a9e55595a2
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-04 23:29:14 +0900

    김초원 : [정렬] 삽입 정렬

---
+++ 이것이 취업을 위한 코딩테스트다/01 삽입 정렬/01 삽입 정렬.cpp
@@ -0,0 +1,31 @@
+﻿// 01 삽입 정렬
+
+#include <iostream>
+using namespace std;
+
+int a[] = { 7,5,9,0,3,1,6,2,4,8 };
+
+int main()
+{
+	int size = sizeof(a) / sizeof(int);
+	int idx = 1;
+
+	for (int i = 0; i < size; i++)
+	{
+		int nIdx = i;
+		for (int j = i - 1; j >= 0; j--)
+		{
+			if (a[nIdx] < a[j])
+			{
+				swap(a[j], a[nIdx]);
+				nIdx = j;
+			}
+			else break;
+		}
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << a[i] << ' ';
+	}
+}
