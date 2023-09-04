bfa6665517585aa63031568891c163f0a5962f2a
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-04 23:30:30 +0900

    김초원 : [정렬] 계수 정렬

---
+++ 이것이 취업을 위한 코딩테스트다/03 계수 정렬/03 계수 정렬.cpp
@@ -0,0 +1,26 @@
+﻿// 03 계수 정렬
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int a[] = { 7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2 };
+
+int main()
+{
+	vector<int> v(10, 0);
+
+	int size = sizeof(a) / sizeof(int);
+	for (int i = 0; i < size; i++)
+	{
+		++v[a[i]];
+	}
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		for (int j = 0; j < v[i]; j++)
+		{
+			cout << i << ' ';
+		}
+	}
+}
