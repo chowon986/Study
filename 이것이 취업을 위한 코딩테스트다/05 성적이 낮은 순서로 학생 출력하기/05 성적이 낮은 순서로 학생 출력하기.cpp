e82e320f9370b72f7be0756571eb3adc8767d97c
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-04 23:31:29 +0900

    김초원 : [정렬] 성적이 낮은 순서로 학생 출력하기

---
+++ 이것이 취업을 위한 코딩테스트다/05 성적이 낮은 순서로 학생 출력하기/05 성적이 낮은 순서로 학생 출력하기.cpp
@@ -0,0 +1,28 @@
+﻿// 05 성적이 낮은 순서로 학생 출력하기
+
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	map<int, string> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		string tString;
+		int tInt;
+		cin >> tString >> tInt;
+		m.insert(make_pair(tInt, tString));
+	}
+
+	for (pair<int, string> it : m)
+	{
+		cout << it.second << ' ';
+	}
+}
