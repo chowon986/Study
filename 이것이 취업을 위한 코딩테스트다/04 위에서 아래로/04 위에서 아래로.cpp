f3b314f67b5c83cd753d07b4a20166e718880c8c
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-04 23:30:59 +0900

    김초원 : [정렬] 위에서 아래로

---
+++ 이것이 취업을 위한 코딩테스트다/04 위에서 아래로/04 위에서 아래로.cpp
@@ -0,0 +1,28 @@
+﻿// 04 위에서 아래로
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector<int> d(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		d[i] = temp;
+	}
+
+	sort(d.begin(), d.end(), greater<>());
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << d[i] << ' ';
+	}
+}
