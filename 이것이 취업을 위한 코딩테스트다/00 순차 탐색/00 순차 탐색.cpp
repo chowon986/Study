0489bc935f0d26370b7c588d3ccae2c63ad656a3
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-04 23:28:28 +0900

    김초원 : [정렬] 순차 탐색

---
+++ 이것이 취업을 위한 코딩테스트다/00 순차 탐색/00 순차 탐색.cpp
@@ -0,0 +1,28 @@
+﻿// 00 순차 탐색
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int n;
+	string target;
+	cin >> n >> target;
+
+	vector<string> v;
+
+	for (int i = 0; i < n; i++)
+	{
+		string temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		if (v[i] == target)
+			cout << i + 1;
+	}
+}
