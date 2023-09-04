a7789ce9234d5387173c58ce6bf4d414b7c4a280
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-04 23:32:17 +0900

    김초원 : [탐색] 이진 탐색

---
+++ 이것이 취업을 위한 코딩테스트다/01 이진 탐색/01 이진 탐색.cpp
@@ -0,0 +1,53 @@
+﻿// 01 이진 탐색
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n, target;
+vector<int> b;
+
+void go(int start, int end)
+{
+	if (start > end)
+	{
+		cout << "원소가 존재하지 않습니다.";
+		return;
+	}
+
+	int mid = (end + start) / 2;
+
+	if (b[mid] == target)
+	{
+		cout << mid + 1;
+		return;
+	}
+	else if (b[mid] < target)
+	{
+		go(mid + 1, end);
+	}
+	else if (b[mid] > target)
+	{
+		go(start, mid - 1);
+	}
+}
+
+int main()
+{
+	cin >> n >> target;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		b.push_back(temp);
+	}
+
+	sort(b.begin(), b.end());
+
+	int start = 0;
+	int end = b.size() - 1;
+
+	go(start, end);
+}
