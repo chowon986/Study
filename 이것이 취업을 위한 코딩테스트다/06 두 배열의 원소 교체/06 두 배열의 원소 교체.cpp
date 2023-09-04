99a10534c7821261d86b83e3eaa568952311b20f
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-04 23:31:48 +0900

    김초원 : [정렬] 두 배열의 원소 교체

---
+++ 이것이 취업을 위한 코딩테스트다/06 두 배열의 원소 교체/06 두 배열의 원소 교체.cpp
@@ -0,0 +1,50 @@
+﻿// 06 두 배열의 원소 교체
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	vector<int> a;
+	vector<int> b;
+
+	int n, k;
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		a.push_back(temp);
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		b.push_back(temp);
+	}
+
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end(), greater<>());
+
+	for (int i = 0; i < k; i++)
+	{
+		if (a[i] < b[i])
+		{
+			swap(a[i], b[i]);
+		}
+	}
+
+	int sum = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		sum += a[i];
+	}
+
+	cout << sum;
+}
