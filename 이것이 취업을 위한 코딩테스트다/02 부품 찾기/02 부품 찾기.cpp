6b5e69bd7a2142e40c2bb2f025e4d06517d98c8f
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-04 23:32:38 +0900

    김초원 : [탐색] 부품 찾기

---
+++ 이것이 취업을 위한 코딩테스트다/02 부품 찾기/02 부품 찾기.cpp
@@ -0,0 +1,85 @@
+﻿// 02 부품 찾기
+
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//
+//	vector<int> mine;
+//	vector<int> order;
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		int temp;
+//		cin >> temp;
+//
+//		mine.push_back(temp);
+//	}
+//
+//	int m;
+//	cin >> m;
+//
+//	for (int i = 0; i < m; i++)
+//	{
+//		int temp;
+//		cin >> temp;
+//
+//		order.push_back(temp);
+//	}
+//
+//	for (int i = 0; i < m; i++)
+//	{
+//		bool ok = false;
+//		for (int j = 0; j < n; j++)
+//		{
+//			if (order[i] == mine[j])
+//			{
+//				cout << "yes" << ' ';
+//				ok = true;
+//				break;
+//			}
+//		}
+//
+//		if (ok == false)
+//			cout << "no" << ' ';
+//	}
+//}
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector<int> mine(1000001, 0);
+	vector<int> order;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+
+		++mine[temp];
+	}
+
+	int m;
+	cin >> m;
+
+	for (int i = 0; i < m; i++)
+	{
+		int temp;
+		cin >> temp;
+
+		if (mine[temp] > 0)
+			cout << "yes" << ' ';
+		else
+			cout << "no" << ' ';
+	}
+}
