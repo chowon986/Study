67f80bc70e2fa2cf5ad8b9ecbd6385f4285d1c0a
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-04 23:32:57 +0900

    김초원 : [탐색] 떡볶이 떡 만들기

---
+++ 이것이 취업을 위한 코딩테스트다/03 떡볶이 떡 만들기/03 떡볶이 떡 만들기.cpp
@@ -0,0 +1,46 @@
+﻿// 03 떡볶이 떡 만들기
+
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//int main()
+//{
+//	int n, m;
+//	cin >> n >> m;
+//
+//	vector<int> b;
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		int temp;
+//		cin >> temp;
+//		b.push_back(temp);
+//	}
+//
+//	sort(b.begin(), b.end(), greater<>());
+//
+//	int maxValue = -1;
+//
+//	for (int i = b[0]; i > 0; i--)
+//	{
+//		int sum = 0;
+//		for (int j = 0; j < b.size(); j++)
+//		{
+//			if (b[j] - i > 0)
+//			{
+//				sum += b[j] - i;
+//			}
+//		}
+//
+//		if (sum >= m)
+//		{
+//			maxValue = max(maxValue, i);
+//		}
+//	}
+//
+//	cout << maxValue;
+//}
+
+// 이진 탐색으로 풀기
