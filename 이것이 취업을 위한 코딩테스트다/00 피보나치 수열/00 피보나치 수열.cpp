d460ca59f93ef7e26f2469a77bde13182a3e15d6
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-05 22:47:43 +0900

    김초원 : [DP] 피보나치 수열

---
+++ 이것이 취업을 위한 코딩테스트다/00 피보나치 수열/00 피보나치 수열.cpp
@@ -0,0 +1,25 @@
+﻿// 00 피보나치 수열
+
+#include <iostream>
+using namespace std;
+
+int d[100];
+
+int go(int n)
+{
+	if (n == 1 || n == 2) return 1;
+
+	if (d[n] != 0) return d[n];
+
+	d[n] = go(n - 1) + go(n - 2);
+
+	return d[n];
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	cout << go(n);
+}
