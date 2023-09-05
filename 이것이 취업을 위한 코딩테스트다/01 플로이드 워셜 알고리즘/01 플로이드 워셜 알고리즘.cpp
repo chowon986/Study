d32ab6b55dab1dbbdcef858442a419ed0955e2db
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-05 22:46:00 +0900

    김초원 : [최단 경로] 플로이드 워셜 알고리즘

---
+++ 이것이 취업을 위한 코딩테스트다/01 플로이드 워셜 알고리즘/01 플로이드 워셜 알고리즘.cpp
@@ -0,0 +1,56 @@
+﻿// 01 플로이드 워셜 알고리즘
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> board;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+
+	board.resize(n + 1, vector<int>(n + 1));
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if (i != j)
+				board[i][j] = 1e9;
+		}
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+
+		board[a][b] = c;
+	}
+
+	for (int k = 1; k <= n; k++)
+	{
+		for (int a = 1; a <= n; a++)
+		{
+			for (int b = 1; b <= n; b++)
+			{
+				board[a][b] = min(board[a][b], board[a][k] + board[k][b]);
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if (board[i][j] == 1e9)
+				cout << "INFINITY" << ' ';
+			else
+				cout << board[i][j] << ' ';
+		}
+
+		cout << '\n';
+	}
+}
