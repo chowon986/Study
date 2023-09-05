4f96aa0eb4d31250766026e7421860d12de89b09
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-05 22:46:39 +0900

    김초원 : [최단 경로] 미래 도시

---
+++ 이것이 취업을 위한 코딩테스트다/02 미래 도시/02 미래 도시.cpp
@@ -0,0 +1,22 @@
+﻿// 02 미래 도시
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+
+	vector<vector<int>> b(n + 1, vector<int>(n + 1));
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+			b[i][j] = 1e9;
+	}
+
+
+	int a = 0;
+}
