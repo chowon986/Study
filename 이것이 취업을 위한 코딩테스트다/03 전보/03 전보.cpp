8626fdfa69624eee2e9b8148c1b01676bc01deaf
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-09-05 22:53:28 +0900

    김초원 : [최단 경로] 전보

---
+++ 이것이 취업을 위한 코딩테스트다/03 전보/03 전보.cpp
@@ -0,0 +1,31 @@
+﻿// 03 전보
+
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+#define INF 1e9
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+
+	vector<vector<pair<int, int>>> board(n + 1);
+	vector<int> distance(n + 1, INF);
+
+	for (int i = 0; i < m; i++)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+		board[a].push_back({ b,c });
+	}
+
+	priority_queue<pair<int, int>> pq;
+
+	int start;
+	cin >> start;
+
+	pq.push({ 0, start });
+}
