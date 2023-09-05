9cd280e70f26fc89087fea2bd59eaf3f8be73454
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-09-05 22:53:49 +0900

    김초원 : 곱하기 혹은 더하기

---
+++ 이것이 취업을 위한 코딩테스트다/05 곱하기 혹은 더하기/05 곱하기 혹은 더하기.cpp
@@ -0,0 +1,25 @@
+﻿// 05 곱하기 혹은 더하기
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string a;
+	cin >> a;
+
+	int sum = 0;
+	for (int i = 0; i < a.size(); i++)
+	{
+		int temp = a[i] - '0';
+		
+		if (i == 0)
+			sum = temp;
+		else if (a[i - 1] == '0') sum += temp;
+		else if (temp == 0) sum += temp;
+		else sum *= temp;
+	}
+
+	cout << sum;
+}
