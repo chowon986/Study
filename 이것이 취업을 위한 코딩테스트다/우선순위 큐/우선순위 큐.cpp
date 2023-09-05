de53df995ff8a34c1ca0b490b681b5ae1e0cd4e7
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-09-05 22:54:22 +0900

    김초원 : 우선순위 큐

---
+++ 이것이 취업을 위한 코딩테스트다/우선순위 큐/우선순위 큐.cpp
@@ -0,0 +1,36 @@
+﻿// 우선순위 큐
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	priority_queue<int> pq;
+
+	pq.push(9);
+	pq.push(8);
+	pq.push(7);
+	pq.push(1);
+
+	while (!pq.empty())
+	{
+		cout << pq.top() << ' ';
+		pq.pop();
+	}
+
+	// 출력 결과 9 8 7 1
+
+	pq.push(-9);
+	pq.push(-8);
+	pq.push(-7);
+	pq.push(-1);
+
+	while (!pq.empty())
+	{
+		cout << pq.top() << ' ';
+		pq.pop();
+	}
+
+	// 출력 결과 -1 -7 -8 -9
+}
