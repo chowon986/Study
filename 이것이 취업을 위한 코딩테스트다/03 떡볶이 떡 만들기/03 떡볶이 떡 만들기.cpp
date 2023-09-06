523a7e35b0ad345c096368a92a4c9fc7bebc5ac7
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-09-06 10:56:32 +0900

    김초원 : [이진 탐색] 떡볶이 떡 만들기

--- 이것이 취업을 위한 코딩테스트다/03 떡볶이 떡 만들기/03 떡볶이 떡 만들기.cpp
+++ 이것이 취업을 위한 코딩테스트다/03 떡볶이 떡 만들기/03 떡볶이 떡 만들기.cpp
@@ -45,9 +45,48 @@
 
 // 이진 탐색으로 풀기
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
+	int n, m;
+	cin >> n >> m;
 
+	vector<int> b;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		b.push_back(temp);
+	}
+
+	sort(b.begin(), b.end(), greater<>());
+
+	int start = 0;
+	int end = b[0];
+	int result = 0;
+	while (start <= end)
+	{
+		int mid = (start + end) / 2;
+		int ttlLong = 0;
+
+		for (int i : b)
+		{
+			if (i > mid)
+				ttlLong += (i - mid);
+		}
+
+		if (ttlLong < m)
+			end = mid - 1;
+		else
+		{
+			result = mid;
+			start = mid + 1;
+		}
+	}
+
+	cout << result;
 }
