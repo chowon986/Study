3d62164e3ee2eaed3eae53424e147a0a8c55b80f
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-09-12 16:09:07 +0900

    김초원 : [DP] 카드 구매하기2

---
+++ 백준 알고리즘/[DP] 카드 구매하기2/[DP] 카드 구매하기2.cpp
@@ -0,0 +1,31 @@
+﻿// [DP] 카드 구매하기2
+
+#include <iostream>
+using namespace std;
+
+int a[1001];
+int d[1001];
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		int temp;
+		cin >> temp;
+		a[i] = temp;
+		d[i] = temp;
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			d[i] = min(d[i], d[i - j] + a[j]);
+		}
+	}
+	
+	cout << d[n];
+}
