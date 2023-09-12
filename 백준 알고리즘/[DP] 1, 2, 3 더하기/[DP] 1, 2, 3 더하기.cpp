db94a594500d46cdf7aeba53e2144300dd8669ed
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-09-12 15:39:12 +0900

    김초원 : [DP] 1, 2, 3 더하기

---
+++ 백준 알고리즘/[DP] 1, 2, 3 더하기/[DP] 1, 2, 3 더하기.cpp
@@ -0,0 +1,27 @@
+﻿// [DP] 1, 2, 3 더하기
+
+#include <iostream>
+using namespace std;
+
+int d[12];
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	d[0] = 0;
+	d[1] = 1;
+	d[2] = 2;
+	d[3] = 4;
+
+	for (int i = 4; i <= 12; i++)
+		d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		cout << d[temp] << '\n';
+	}
+}
