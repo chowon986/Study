510b7e62422209f9049e3e5d4e793e253041fdff
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-13 20:45:40 +0900

    김초원 : [큐와 그래프] 이분 그래프

---
+++ 백준 알고리즘/[큐와 그래프] 이분 그래프/[큐와 그래프] 이분 그래프.cpp
@@ -0,0 +1,75 @@
+﻿// [큐와 그래프] 이분 그래프
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector<int> a[20001];
+int color[20001];
+
+bool dfs(int node, int c) // node에 방문, node의 색
+{
+	color[node] = c;
+
+	for (int i = 0; i < a[node].size(); i++)
+	{
+		int next = a[node][i];
+		if (color[next] == 0)
+		{
+			if (dfs(next, 3 - c) == false) // 이분그래프가 아니면
+			{
+				return false;
+			}
+		}
+		else if (color[next] == color[node])
+		{
+			return false;
+		}
+
+	}
+
+	return true;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int n, m;
+		cin >> n >> m;
+
+		for (int i = 1; i <= n; i++)
+		{
+			a[i].clear();
+			color[i] = 0;
+		}
+
+		for (int i = 0; i < m; i++)
+		{
+			int u, v;
+			cin >> u >> v;
+			a[u].push_back(v);
+			a[v].push_back(u);
+		}
+		bool ok = true;
+		for (int i = 1; i <= n; i++)
+		{
+			if (color[i] == 0)
+			{
+				if (dfs(i, 1) == false)
+				{
+					ok = false;
+				}
+			}
+		}
+
+		string ans = ok == true ? "YES" : "NO";
+		cout << ans << '\n';
+	}
+}
