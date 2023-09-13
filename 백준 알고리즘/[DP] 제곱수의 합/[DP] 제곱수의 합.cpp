723f104f4162547e9cacb66befafba72d1d8fb82
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-09-13 14:25:30 +0900

    김초원 : [DP] 제곱수의 합

---
+++ 백준 알고리즘/[DP] 제곱수의 합/[DP] 제곱수의 합.cpp
@@ -0,0 +1,24 @@
+﻿// [DP] 제곱수의 합
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+	int N = 0;
+	cin >> N;
+	vector<int> D(N + 1, 0);
+
+	for (int i = 1; i <= N; i++)
+	{
+		D[i] = i; // 모든 수를 1로 채운 경우 -> Max 케이스
+		for (int j = 1; j * j <= i; j++)
+		{
+			D[i] = min(D[i], D[i - j * j] + 1);
+		}
+	}
+
+	cout << D[N];
+}
