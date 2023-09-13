c1fc577848d19f3d2799793f8970b152eb11ae75
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-09-13 13:44:24 +0900

    김초원 : [DP] 연속합

---
+++ 백준 알고리즘/[DP] 연속합/[DP] 연속합.cpp
@@ -0,0 +1,32 @@
+﻿// [DP] 연속합
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector<int> a(n);
+	vector<int> d(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		a[i] = temp;
+	}
+
+	d[0] = a[0];
+	int maxValue = d[0];
+
+	for (int i = 1; i < n; i++)
+	{
+		d[i] = max(a[i], d[i - 1] + a[i]);
+		maxValue = max(maxValue, d[i]);
+	}
+
+	cout << maxValue;
+}
