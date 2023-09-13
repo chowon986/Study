9489565eb944f6ec927b5ddfdaac1066dae19cb2
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-09-13 16:39:08 +0900

    김초원 : [DP] 연속합 2

---
+++ 백준 알고리즘/[DP] 연속합 2/[DP] 연속합 2.cpp
@@ -0,0 +1,34 @@
+﻿// [DP] 연속합 2
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int DP[100001][2];
+int Arr[100001];
+
+int main()
+{
+	int Cnt;
+	cin >> Cnt;
+
+	for (int i = 0; i < Cnt; i++)
+	{
+		cin >> Arr[i];
+	}
+
+	DP[0][0] = Arr[0];
+	DP[0][1] = Arr[0];
+
+	int Max = Arr[0];
+
+	for (int i = 1; i < Cnt; i++)
+	{
+		DP[i][0] = max(DP[i - 1][0] + Arr[i], Arr[i]);
+		DP[i][1] = max(DP[i - 1][0], DP[i - 1][1] + Arr[i]);
+
+		Max = max(Max, max(DP[i][0], DP[i][1]));
+	}
+
+	cout << Max;
+
+}
