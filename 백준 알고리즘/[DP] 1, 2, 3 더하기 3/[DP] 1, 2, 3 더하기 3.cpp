08168c03b66a090455e5ad8453427f1a909bb1f6
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-09-13 14:25:16 +0900

    김초원 : [DP] 1, 2, 3 더하기 3

---
+++ 백준 알고리즘/[DP] 1, 2, 3 더하기 3/[DP] 1, 2, 3 더하기 3.cpp
@@ -0,0 +1,31 @@
+﻿// [DP] 1, 2, 3 더하기 3
+
+#include <iostream>
+using namespace std;
+
+#define MOD 1000000009
+#define MAX 1000000
+
+int d[MAX + 1];
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	d[1] = 1;
+	d[2] = 2;
+	d[3] = 4;
+
+	for(int i = 4; i <= MAX; i++)
+	{
+		d[i] = (d[i - 1] + d[i - 2] + d[i - 3]) % MOD;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		cout << d[temp] << '\n';
+	}
+}
