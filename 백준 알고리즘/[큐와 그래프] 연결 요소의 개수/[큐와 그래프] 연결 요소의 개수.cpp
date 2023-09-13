e06d3238287ee0fa2149df78966c0d215eb0f96f
chowon986 <97011721+chowon986@users.noreply.github.com>
2023-09-13 20:27:42 +0900

    김초원 : [큐와 그래프] 연결 요소의 개수

---
+++ 백준 알고리즘/[큐와 그래프] 연결 요소의 개수/[큐와 그래프] 연결 요소의 개수.cpp
@@ -0,0 +1,72 @@
+﻿// [큐와 그래프] 연결 요소의 개수
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> board;
+vector<bool> isVisited;
+bool isComponent;
+int n, m;
+void dfs(int x)
+{
+	isVisited[x] = true;
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (board[x][i] == 1 && isVisited[i] == false)
+		{
+			isComponent = true;
+			dfs(i);
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> n >> m;
+
+	board.resize(n + 1, vector<int>(n + 1));
+	isVisited.resize(n + 1);
+
+	for (int i = 0; i < m; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		board[a][b] = 1;
+		board[b][a] = 1;
+	}
+
+	int count = 0;
+	
+	// 이어지는 정점의 개수가 없는 경우
+	for (int i = 1; i <= n; i++)
+	{
+		bool hasComponent = false;
+		for (int j = 1; j <= n; j++)
+		{
+			if (board[i][j] == 1) 
+			{
+				hasComponent = true;
+				break;
+			}
+		}
+
+		if (hasComponent == false)
+			++count;
+	}
+	
+	for (int i = 1; i <= n; i++)
+	{
+		isComponent = false;
+		dfs(i);
+
+		if (isComponent == true)
+			++count;
+	}
+
+	cout << count;
+}
