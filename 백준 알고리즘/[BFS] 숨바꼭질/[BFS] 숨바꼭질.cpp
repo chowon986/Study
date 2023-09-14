1cd1f147b910be09a9c7c7242bc4f44b58262437
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-09-14 13:59:30 +0900

    김초원 : [BFS] 숨바꼭질

---
+++ 백준 알고리즘/[BFS] 숨바꼭질/[BFS] 숨바꼭질.cpp
@@ -0,0 +1,63 @@
+﻿// [BFS] 숨바꼭질
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+#define MAX 200000
+
+int d[MAX + 1];
+bool isVisited[MAX + 1];
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+
+	queue<int> q;
+	q.push(n);
+	isVisited[n] = true;
+
+	while (!q.empty())
+	{
+		int now = q.front();
+
+		if (now == m)
+		{
+			cout << d[m];
+			return 0;
+		}
+		
+		q.pop();
+
+		if (now + 1 <= MAX)
+		{
+			if (isVisited[now + 1] == false)
+			{
+				d[now + 1] = d[now] + 1;
+				q.push(now + 1);
+				isVisited[now + 1] = true;
+			}
+		}
+
+		if (now - 1 >= 0)
+		{
+			if (isVisited[now - 1] == false)
+			{
+				d[now - 1] = d[now] + 1;
+				q.push(now - 1);
+				isVisited[now - 1] = true;
+			}
+		}
+
+		if (now * 2 <= MAX)
+		{
+			if (isVisited[now * 2] == false)
+			{
+				d[now * 2] = d[now] + 1;
+				q.push(now * 2);
+				isVisited[now * 2] = true;
+			}
+		}
+	}
+}
