781e489fa25909e5fd2f5ba3afb25d2b40be5ebd
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-09-14 14:13:15 +0900

    김초원 : [BFS] 숨바꼭질 4

---
+++ 백준 알고리즘/[BFS] 숨바꼭질 4/[BFS] 숨바꼭질 4.cpp
@@ -0,0 +1,77 @@
+﻿// [BFS] 숨바꼭질 4
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+#define MAX 200000
+
+vector<bool> isVisited(MAX + 1);
+vector<int> prevVisited(MAX + 1, -1);
+vector<int> d(MAX + 1);
+
+void PrintVisitSpot(int now)
+{
+	if (now == -1) return;
+
+	PrintVisitSpot(prevVisited[now]);
+	cout << now << ' ';
+}
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+
+	queue<int> q;
+	q.push(n);
+	isVisited[n] = true;
+	
+	while (!q.empty())
+	{
+		int now = q.front();
+
+		if (now == m)
+		{
+			cout << d[m] << '\n';
+			PrintVisitSpot(m);
+			return 0;
+		}
+
+		q.pop();
+
+		if (now + 1 <= MAX)
+		{
+			if (isVisited[now + 1] == false)
+			{
+				d[now + 1] = d[now] + 1;
+				isVisited[now + 1] = true;
+				q.push(now + 1);
+				prevVisited[now + 1] = now;
+			}
+		}
+
+		if (now - 1 >= 0)
+		{
+			if (isVisited[now - 1] == false)
+			{
+				d[now - 1] = d[now] + 1;
+				isVisited[now - 1] = true;
+				q.push(now - 1);
+				prevVisited[now - 1] = now;
+			}
+		}
+
+		if (now * 2 <= MAX)
+		{
+			if (isVisited[now * 2] == false)
+			{
+				d[now * 2] = d[now] + 1;
+				isVisited[now * 2] = true;
+				q.push(now * 2);
+				prevVisited[now * 2] = now;
+			}
+		}
+	}
+}
