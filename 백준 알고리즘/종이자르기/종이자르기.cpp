3799b8e79e749e5b7845f4e91fc12dc66718c7ae
KimChowon <chowon986@gmail.com>
2023-10-20 09:44:36 +0900

    김초원 : [수학] 종이자르기

---
+++ 백준 알고리즘/종이자르기/종이자르기.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+	int n, m, cnt;
+	cin >> n >> m >> cnt;
+
+	vector<int> horizontal;
+	vector<int> vertical;
+
+	horizontal.push_back(0);
+	horizontal.push_back(m);
+	vertical.push_back(0);
+	vertical.push_back(n);
+
+	for (int i = 0; i < cnt; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		if (a == 0)
+			horizontal.push_back(b);
+		else
+			vertical.push_back(b);
+	}
+
+	sort(horizontal.begin(), horizontal.end());
+	sort(vertical.begin(), vertical.end());
+
+	int maxSize = 0;
+	for (int i = 1; i < vertical.size(); i++)
+	{
+		int x = 0; int y = 0;
+
+		x = vertical[i] - vertical[i - 1];
+
+		for (int j = 1; j < horizontal.size(); j++)
+		{
+			y = horizontal[j] - horizontal[j - 1];
+			maxSize = max(maxSize, x * y);
+		}
+	}
+
+	cout << maxSize;
+}
