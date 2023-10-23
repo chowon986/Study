95dc0205cccfcd478ec51c9f94959f29647bfd4a
KimChowon <chowon986@gmail.com>
2023-10-23 18:42:46 +0900

    김초원 : [스터디 4주차] 행렬 덧셈

---
+++ 백준 알고리즘/최댓값/최댓값.cpp
@@ -0,0 +1,35 @@
+﻿// 최댓값.cpp 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define MAX 9
+
+vector<vector<int>> board(MAX, vector<int>(MAX));
+
+int main()
+{
+	int maxValue = -1;
+	int row = -1;
+	int col = -1;
+
+	for (int i = 0; i < MAX; i++)
+	{
+		for (int j = 0; j < MAX; j++)
+		{
+			int temp;
+			cin >> temp;
+			board[i][j] = temp;
+
+			if (temp > maxValue)
+			{
+				maxValue = temp;
+				row = i + 1;
+				col = j + 1;
+			}
+		}
+	}
+
+	cout << maxValue << '\n';
+	cout << row << ' ' << col;
+}
