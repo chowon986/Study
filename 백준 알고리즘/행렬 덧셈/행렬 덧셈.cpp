d81f8602951c2adf7bce372efd98b59d404e9997
KimChowon <chowon986@gmail.com>
2023-10-23 18:46:42 +0900

    김초원 : [스터디 4주차] 최댓값

---
+++ 백준 알고리즘/행렬 덧셈/행렬 덧셈.cpp
@@ -0,0 +1,48 @@
+﻿// 행렬 덧셈.cpp 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> board1;
+vector<vector<int>> board2;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+
+	board1.resize(n, vector<int>(m));
+	board2.resize(n, vector<int>(m));
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			int temp;
+			cin >> temp;
+
+			board1[i][j] = temp;
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			int temp;
+			cin >> temp;
+
+			board2[i][j] = temp;
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << board1[i][j] + board2[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+}
