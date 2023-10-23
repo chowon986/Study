c5fe206d0671b26ce553091b350da51228f41c3b
KimChowon <chowon986@gmail.com>
2023-10-23 11:00:22 +0900

    김초원 : [스터디 4주차] 단어 공부

---
+++ 백준 알고리즘/단어 공부/단어 공부.cpp
@@ -0,0 +1,39 @@
+﻿// 단어 공부.cpp 
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> alphabet(26, 0);
+
+int main()
+{
+	string s;
+	cin >> s;
+
+	int maxCount = 0;
+
+	for (int i = 0; i < s.size(); i++)
+	{
+		int temp = toupper(s[i]) - 'A';
+		++alphabet[temp];
+
+		maxCount = max(maxCount, alphabet[temp]);
+	}
+
+	int count = 0;
+	int num = 0;
+	for (int i = 0; i < 26; i++)
+	{
+		if (alphabet[i] == maxCount)
+		{
+			++count;
+			num = i;
+		}
+	}
+
+	if (count >= 2) cout << '?';
+	else cout << (char)(num + 'A');
+
+}
