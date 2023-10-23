6a5a9e9bc6d5e13262343a9a712c8ed795d48eae
KimChowon <chowon986@gmail.com>
2023-10-23 09:39:17 +0900

    김초원 : [스터디 4주차] 별 찍기 - 7

---
+++ 백준 알고리즘/별 찍기 - 7/별 찍기 - 7.cpp
@@ -0,0 +1,32 @@
+﻿// 별 찍기 - 7.cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int blank = 1;
+	int j = 1;
+	for (; j < n * 2; j += 2)
+	{
+		for (int k = blank; k < n; k++) cout << ' ';
+		for (int i = 0; i < j; i++) cout << '*';
+
+		cout << '\n';
+		++blank;
+	}
+
+	j -= 4;
+
+	for (; j > 0; j -= 2)
+	{
+		for (int k = blank; k > n; k--) cout << ' ';
+		for (int i = 0; i < j; i++) cout << '*';
+
+		cout << '\n';
+		blank++;
+	}
+}
