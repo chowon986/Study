8c3608a3d836c6e5765c80a701df9cd79df4740d
KimChowon <chowon986@gmail.com>
2023-10-23 18:25:50 +0900

    김초원 : [스터디 4주차] 세로읽기

---
+++ 백준 알고리즘/세로읽기/세로읽기.cpp
@@ -0,0 +1,30 @@
+﻿// 세로읽기.cpp 
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<string> words;
+
+	for (int i = 0; i < 5; i++)
+	{
+		string temp;
+		cin >> temp;
+		words.push_back(temp);
+	}
+
+	int j = 0;
+
+	for (int i = 0; i < 15; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if(words[j].size() > i)
+				cout << words[j][i];
+		}
+	}
+}
