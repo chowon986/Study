ade10b55509b728705adc2ef3a3d095163cc0440
KimChowon <chowon986@gmail.com>
2023-10-23 19:18:04 +0900

    김초원 : [스터디 4주차] 그룹 단어 체커

---
+++ 백준 알고리즘/그룹 단어 체커/그룹 단어 체커.cpp
@@ -0,0 +1,57 @@
+﻿// 그룹 단어 체커.cpp 
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<bool> alphabet(26);
+
+void reset()
+{
+	for (int i = 0; i < 26; i++)
+		alphabet[i] = false;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector<string> words;
+
+	for (int i = 0; i < n; i++)
+	{
+		string temp;
+		cin >> temp;
+
+		words.push_back(temp);
+	}
+
+	int count = 0;
+	for (int i = 0; i < n; i++)
+	{
+		reset();
+		bool isFail = false;
+		char prevWord = words[i][0];
+		alphabet[words[i][0] - 'a'] = true;
+		for (int j = 1; j < words[i].size(); j++)
+		{
+			if (prevWord != words[i][j])
+			{
+				if (alphabet[words[i][j] - 'a'] == true)
+				{
+					isFail = true;
+					break;
+				}
+				
+				prevWord = words[i][j];
+				alphabet[words[i][j] - 'a'] = true;
+			}
+		}
+
+		if (!isFail) ++count;
+	}
+
+	cout << count;
+}
