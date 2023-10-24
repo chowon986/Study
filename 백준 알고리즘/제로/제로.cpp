b81baa1634fc826fb8aeb55dfb915b8a8985c1a1
KimChowon <chowon986@gmail.com>
2023-10-24 19:46:40 +0900

    김초원 : [스터디 4주차] 제로

---
+++ 백준 알고리즘/제로/제로.cpp
@@ -0,0 +1,32 @@
+﻿// 제로.cpp
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	stack<int> s;
+
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		if (temp == 0)
+		{
+			sum -= s.top();
+			s.pop();
+		}
+		else
+		{
+			sum += temp;
+			s.push(temp);
+		}
+	}
+
+	cout << sum;
+}
