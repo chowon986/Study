20a174473f57d0a8da00633876d6519cf2a5e003
KimChowon <chowon986@gmail.com>
2023-10-26 20:44:17 +0900

    김초원 : [스터디 5주차] 블랙잭

---
+++ 백준 알고리즘/블랙잭/블랙잭.cpp
@@ -0,0 +1,38 @@
+﻿// 블랙잭.cpp
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+
+	int sum = 100000000;
+	vector<int> numbers(n, 0);
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		numbers[i] = temp;
+	}
+
+	for (int i = 0; i < n - 2; i++)
+	{
+		for (int j = i + 1; j < n - 1; j++)
+		{
+			for (int k = j + 1; k < n; k++)
+			{
+				int temp = numbers[i] + numbers[j] + numbers[k];
+				if (m >= temp && (m - temp) < abs(m - sum))
+				{
+					sum = temp;
+				}
+			}
+		}
+	}
+
+	cout << sum;
+}
