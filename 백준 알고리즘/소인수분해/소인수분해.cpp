edcfed153e05d506476a42bb230906cfa263a31d
KimChowon <chowon986@gmail.com>
2023-10-30 19:43:59 +0900

    김초원 : [스터디 5주차] 소인수분해

---
+++ 백준 알고리즘/소인수분해/소인수분해.cpp
@@ -0,0 +1,33 @@
+﻿// 소인수분해
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector<int> ans;
+
+	while (n != 1)
+	{
+		for (int i = 2; i <= n; i++)
+		{
+			if (n % i == 0)
+			{
+				ans.push_back(i);
+				n /= i;
+				break;
+			}
+		}
+	}
+
+	sort(ans.begin(), ans.end());
+
+	for (int i : ans)
+		cout << i << '\n';
+}
