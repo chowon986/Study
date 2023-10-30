5290e4a303d86101ce1eed90a0724a72042bd0d1
KimChowon <chowon986@gmail.com>
2023-10-30 19:37:25 +0900

    김초원 : [스터디 5주차] 벌집

---
+++ 백준 알고리즘/벌집/벌집.cpp
@@ -0,0 +1,18 @@
+﻿// 벌집.cpp
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int temp = 1;
+	int idx = 1;
+	while (temp < n)
+		temp += (6 * idx++);
+
+	cout << idx;
+}
