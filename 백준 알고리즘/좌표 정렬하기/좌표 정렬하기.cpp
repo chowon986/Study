8e8860153f6238993d0bc1d8b4efd9f565c91425
KimChowon <chowon986@gmail.com>
2023-10-30 21:07:48 +0900

    김초원 : [스터디 5주차] 좌표 정렬하기

---
+++ 백준 알고리즘/좌표 정렬하기/좌표 정렬하기.cpp
@@ -0,0 +1,39 @@
+﻿// 좌표 정렬하기
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool cmp(pair<int, int>& a, pair<int, int>& b)
+{
+	if (a.first == b.first)
+		return a.second < b.second;
+
+	return a.first < b.first;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	vector<pair<int, int>> numbers(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		numbers[i] = make_pair(a, b);
+	}
+
+	sort(numbers.begin(), numbers.end(),cmp);
+	
+	for (int i = 0; i < numbers.size(); i++)
+	{
+		cout << numbers[i].first << ' ' << numbers[i].second << '\n';
+	}
+}
