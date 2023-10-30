0c23eb9c4a0463dc1b4d8545d5f5637ff7637e21
KimChowon <chowon986@gmail.com>
2023-10-30 19:32:36 +0900

    김초원 : [스터디 5주차] 진법 변환

---
+++ 백준 알고리즘/진법 변환/진법 변환.cpp
@@ -0,0 +1,28 @@
+﻿// 진법 변환.cpp
+
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    string n;
+    int b;
+    cin >> n >> b;
+
+    int j = 0;
+    unsigned int sum = 0;
+
+    for (int i = n.size() - 1; i >= 0; i--)
+    {
+        int temp = n[i] - '0';
+        if (n[i] >= 'A')
+            temp = n[i] - '7';
+
+		sum += temp * pow(b, j++);
+    }
+
+    cout << sum;
+}
