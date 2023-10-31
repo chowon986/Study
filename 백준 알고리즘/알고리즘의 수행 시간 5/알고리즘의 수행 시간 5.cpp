a1f482aeb680b7469aa7a7b68c3ac8484bbbc8f2
KimChowon <chowon986@gmail.com>
2023-10-31 14:13:19 +0900

    김초원 : [스터디 5주차] 알고리즘의 수행 시간 5

---
+++ 백준 알고리즘/알고리즘의 수행 시간 5/알고리즘의 수행 시간 5.cpp
@@ -0,0 +1,15 @@
+﻿// 알고리즘의 수행 시간 5
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	long long n;
+	cin >> n;
+	cout << n * n * n << '\n' << 3 << '\n';
+}
