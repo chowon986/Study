503acad1e966279e1e5510461924cd5980f2cfdc
KimChowon <chowon986@gmail.com>
2023-10-31 16:09:27 +0900

    김초원 : [5주차 스터디] 팰린드롬수

---
+++ 백준 알고리즘/팰린드롬수/팰린드롬수.cpp
@@ -0,0 +1,32 @@
+﻿// 팰린드롬수
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	while (true)
+	{
+		string temp;
+		cin >> temp;
+
+		if (temp == "0")
+			return 0;
+
+		int lastIdx = temp.size() - 1;
+		bool isTrue = true;
+		for (int i = 0; i < temp.size() / 2; i++)
+		{
+			if (temp[i] != temp[lastIdx--])
+			{
+				cout << "no" << '\n';
+				isTrue = false;
+				break;
+			}
+		}
+
+		if (isTrue)
+			cout << "yes" << '\n';
+	}
+}
