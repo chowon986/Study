78b3bb76c4c39d21ba08a6e124cee5fe43209629
KimChowon <chowon986@gmail.com>
2023-10-31 15:44:35 +0900

    김초원 [스터디 5주차] 좌표 압축

---
+++ 백준 알고리즘/좌표 압축/좌표 압축.cpp
@@ -0,0 +1,33 @@
+﻿// 좌표 압축
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	vector<int> numbers;
+	vector<int> sortedNumbers;
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		numbers.push_back(temp);
+		sortedNumbers.push_back(temp);
+	}
+
+	sort(sortedNumbers.begin(), sortedNumbers.end());
+	sortedNumbers.erase(unique(sortedNumbers.begin(), sortedNumbers.end()), sortedNumbers.end());
+
+	for (int i : numbers)
+		cout << lower_bound(sortedNumbers.begin(), sortedNumbers.end(), i) - sortedNumbers.begin() << ' ';
+}
