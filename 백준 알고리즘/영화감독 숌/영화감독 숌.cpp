6412c1e6b33d5a3427516a85b0a4400e632b116b
KimChowon <chowon986@gmail.com>
2023-10-31 14:21:10 +0900

    김초원 : [스터디 5주차] 영화감독 숌

---
+++ 백준 알고리즘/영화감독 숌/영화감독 숌.cpp
@@ -0,0 +1,39 @@
+﻿// 영화감독 숌.cpp 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int curFilmName = 666;
+	int curCount = 1;
+
+	while (curCount != n)
+	{
+		++curFilmName;
+
+		string temp = to_string(curFilmName);
+
+		int cnt = 0;
+		for (int i = 0; i < temp.size(); i++)
+		{
+			if (temp[i] == '6')
+			{
+				++cnt;
+				if (cnt == 3)
+					break;
+			}
+			else
+				cnt = 0;
+		}
+
+		if (cnt == 3)
+			++curCount;
+	}
+
+	cout << curFilmName;
+}
