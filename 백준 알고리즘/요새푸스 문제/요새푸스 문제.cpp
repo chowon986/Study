9eaa190474e22b7b1f6bd31f7679da3f0c1a022f
KimChowon <chowon986@gmail.com>
2023-11-03 10:52:47 +0900

    김초원 : [스터디 6주차] 요세푸스 문제

---
+++ 백준 알고리즘/요새푸스 문제/요새푸스 문제.cpp
@@ -0,0 +1,42 @@
+﻿// 요새푸스 문제
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+
+	queue<int> numbers;
+
+	for (int i = 1; i <= n; i++)
+		numbers.push(i);
+
+	int cnt = 0;
+
+	cout << '<';
+
+	while (!numbers.empty())
+	{
+		++cnt;
+		if (cnt < k)
+		{
+			numbers.push(numbers.front());
+			numbers.pop();
+		}
+		else if (cnt == k)
+		{
+			if (numbers.size() != 1)
+				cout << numbers.front() << ", ";
+			else
+				cout << numbers.front();
+			numbers.pop();
+
+			cnt = 0;
+		}
+	}
+
+	cout << '>';
+}
