3fb3d4c9da1a89b75c88b93f689c40c2d6549705
KimChowon <chowon986@gmail.com>
2023-11-03 14:55:10 +0900

    김초원 : [스터디 6주차] 단어 뒤집기2

---
+++ 백준 알고리즘/단어 뒤집기2/단어 뒤집기2.cpp
@@ -0,0 +1,50 @@
+﻿// 단어 뒤집기2.cpp
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	string s;
+	getline(cin, s);
+
+	string curString;
+	bool check = false;
+	for (int i = 0; i <= s.size(); i++)
+	{
+		if (s[i] == '<')
+			check = true;
+
+		if (i == s.size())
+		{
+			reverse(curString.begin(), curString.end());
+			cout << curString << ' ';
+		}
+		else if (s[i] == '>')
+		{
+			int temp = find(curString.begin(), curString.end(), '<') - curString.begin();
+
+			for (int i = temp - 1; i >=0; i--)
+				cout << curString[i];
+
+			for (int i = temp; i < curString.size(); i++)
+				cout << curString[i];
+
+			cout << '>';
+			curString = "";
+			check = false;
+		}
+
+		else if (s[i] == ' ' && !check)
+		{
+			reverse(curString.begin(), curString.end());
+			cout << curString << ' ';
+			curString = "";
+		}
+		
+		else
+			curString += s[i];
+	}
+}
