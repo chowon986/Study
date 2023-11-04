686b035a2c7483b66deed4cb8717a7b5b227dd40
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-11-04 20:53:41 +0900

    김초원 : [스터디 6주차] 쇠막대기

---
+++ 백준 알고리즘/쇠막대기/쇠막대기.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    getline(cin, s);
+    stack<char> cs;
+
+    int cnt = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == ')')
+        {
+            if (cs.size() > 0)
+            {
+                if (s[i - 1] == ')')
+                {
+                    ++cnt;
+                    cs.pop();
+                }
+                else
+                {
+                    cs.pop();
+                    cnt += cs.size();
+                }
+            }
+        }
+        else
+            cs.push('(');
+    }
+
+    cout << cnt;
+}
