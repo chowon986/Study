d3fc82f8dec5dda24f386cbd0ed64fe99233efb5
KimChowon <chowon986@gmail.com>
2023-11-06 10:08:00 +0900

    김초원 : [스터디 6주차] 후위 표기식

---
+++ 백준 알고리즘/후위 표기식/후위 표기식.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    stack<char> operators;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] >= 'A' && s[i] <= 'Z')
+            cout << s[i];
+        else
+        {
+            if (s[i] == ')')
+            {
+                while (operators.top() != '(')
+                {
+                    cout << operators.top();
+                    operators.pop();
+                }
+                operators.pop();
+            }
+            else if (s[i] == '(')
+                operators.push(s[i]);
+            else if (s[i] == '*' || s[i] == '/')
+            {
+                while (!operators.empty() && (operators.top() == '/' || operators.top() == '*'))
+                {
+                    cout << operators.top();
+                    operators.pop();
+                }
+                operators.push(s[i]);
+            }
+            else if (s[i] == '+' || s[i] == '-')
+            {
+                while (!operators.empty() && operators.top() != '(')
+                {
+                    cout << operators.top();
+                    operators.pop();
+                }
+                operators.push(s[i]);
+            }
+        }
+    }
+
+    while (!operators.empty())
+    {
+        cout << operators.top();
+        operators.pop();
+    }
+}
