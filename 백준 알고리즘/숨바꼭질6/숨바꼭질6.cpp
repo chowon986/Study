168da9122942aec8c9c7d489f58dc5b49aef0df6
KimChowon <chowon986@gmail.com>
2023-11-13 09:51:00 +0900

    김초원 : [스터디 6주차] 숨바꼭질6

---
+++ 백준 알고리즘/숨바꼭질6/숨바꼭질6.cpp
@@ -0,0 +1,43 @@
+﻿// 숨바꼭질6.cpp 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<unsigned int> v;
+
+unsigned int gcd(unsigned int a, unsigned int b)
+{
+	if (b == 0) return a;
+
+	return gcd(b, a % b);
+}
+
+int main()
+{
+	int n, s;
+	cin >> n >> s;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(abs(temp - s));
+	}
+
+	if (n == 1)
+	{
+		cout << v[0];
+		return 0;
+	}
+
+	sort(v.begin(), v.end());
+
+	unsigned int curNum = gcd(v[0], v[1]);
+
+	for (int i = 2; i < n; i++)
+		curNum = gcd(curNum, v[i]);
+
+	cout << curNum;
+}
