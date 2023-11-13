c5e7db0dac8a56271f126867f2e0761c22a7f40e
KimChowon <chowon986@gmail.com>
2023-11-13 09:55:25 +0900

    김초원 : 1로 만들기

---
+++ 백준 알고리즘/1로 만들기_/1로 만들기_.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int d[1000001];
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+
+	// 점화식 d[6] = d[6/3] + 1 이랑 d[6/2] + 1 이랑 d[6-1] + 1중에 가장 작은 값
+
+	for (int i = 2; i <= n; i++)
+	{
+		int tempCount = 1e9;
+		if (i % 2 == 0)
+			tempCount = d[i / 2] + 1;
+		if (i % 3 == 0)
+			tempCount = min(d[i / 3] + 1, tempCount);
+		if (i - 1 >= 1)
+			tempCount = min(d[i - 1] + 1, tempCount);
+
+		d[i] = tempCount;
+	}
+
+	cout << d[n];
+}
