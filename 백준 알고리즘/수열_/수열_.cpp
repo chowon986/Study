5a4d615203cbd86f339512c13da83ed34ed014c1
KimChowon <chowon986@gmail.com>
2023-12-05 13:57:50 +0900

    김초원 : 수열

---
+++ 백준 알고리즘/수열_/수열_.cpp
@@ -0,0 +1,35 @@
+﻿// 수열_.cpp
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	queue<int> q;
+
+	int n, k;
+	cin >> n >> k;
+
+	int maxValue = -10000000;
+	int sum = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+
+		if (q.size() < k)
+		{
+			q.push(temp);
+			sum += temp;
+		}
+		if (q.size() == k)
+		{
+			maxValue = max(maxValue, sum);
+			sum -= q.front();
+			q.pop();
+		}
+	}
+
+	cout << maxValue;
+}
