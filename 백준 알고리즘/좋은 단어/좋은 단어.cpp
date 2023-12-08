f25ade84fdc8eead3633d78e785a3faaddd2d84c
KimChowon <chowon986@gmail.com>
2023-12-08 17:29:56 +0900

    김초원 : 좋은 단어

---
+++ 백준 알고리즘/좋은 단어/좋은 단어.cpp
@@ -0,0 +1,40 @@
+﻿// 좋은 단어
+
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int count = 0;
+	for (int i = 0; i < n; i++)
+	{
+		string temp;
+		cin >> temp;
+		stack<char> s;
+
+		int size = temp.size();
+
+		for(int j = 0; j < size; j++)
+		{
+			if (!s.empty())
+			{
+				if (s.top() == temp[j])
+					s.pop();
+				else
+					s.push(temp[j]);
+			}
+			else
+				s.push(temp[j]);
+		}
+
+		if (s.empty())
+			++count;
+	}
+
+	cout << count;
+}
