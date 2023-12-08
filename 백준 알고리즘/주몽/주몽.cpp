8e1851f4a432601bea69f965a87787c44c5af125
KimChowon <chowon986@gmail.com>
2023-12-08 16:27:06 +0900

    김초원 : 주몽

---
+++ 백준 알고리즘/주몽/주몽.cpp
@@ -0,0 +1,47 @@
+﻿// 주몽.cpp
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+
+	vector<int> v;
+	int startIdx = 0;
+	int endIdx = n - 1;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	sort(v.begin(), v.end());
+
+	int count = 0;
+	while (startIdx < endIdx)
+	{
+		int temp = v[startIdx] + v[endIdx];
+		if (temp == m)
+		{
+			++count;
+			++startIdx;
+			--endIdx;
+		}
+		else if (temp > m)
+		{
+			--endIdx;
+		}
+		else
+		{
+			++startIdx;
+		}
+	}
+
+	cout << count;
+}
