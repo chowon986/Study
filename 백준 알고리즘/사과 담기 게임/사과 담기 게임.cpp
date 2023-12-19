6107c1a81735a4685ecef9c5f22468127be7c9bd
KimChowon <chowon986@gmail.com>
2023-12-19 16:32:46 +0900

    김초원 : 사과 담기 게임

---
+++ 백준 알고리즘/사과 담기 게임/사과 담기 게임.cpp
@@ -0,0 +1,44 @@
+﻿// 사과 담기 게임
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, j;
+	cin >> n >> m >> j;
+
+	int startIdx = 1;
+	int endIdx = m;
+	int answer = 0;
+
+	for (int i = 0; i < j; i++)
+	{
+		int temp;
+		cin >> temp;
+		int count = 0;
+
+		while (true)
+		{
+			if (temp >= startIdx && temp <= endIdx)
+				break;
+			
+			if (temp > endIdx && endIdx < n)
+			{
+				++startIdx;
+				++endIdx;
+				++count;
+			}
+			else if (temp < startIdx && startIdx > 1)
+			{
+				--startIdx;
+				--endIdx;
+				++count;
+			}
+		}
+
+		answer += count;
+	}
+
+	cout << answer;
+}
