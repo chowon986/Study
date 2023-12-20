90270d8d8f7a2560d0cafd47ab54dec24d48e342
KimChowon <chowon986@gmail.com>
2023-12-20 17:01:48 +0900

    김초원 : 교수가 된 현우

---
+++ 백준 알고리즘/교수가 된 현우/교수가 된 현우.cpp
@@ -0,0 +1,45 @@
+﻿// 교수가 된 현우
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+
+		int copyTemp = temp;
+		int ans = 0;
+
+		// 숫자를 5로 나눈다.
+		temp /= 5;
+		ans += temp;
+
+		// 숫자를 25로 나눈다.
+		copyTemp /= 25;
+		ans += copyTemp;
+
+		// 25로 나누어지면 더 다눈다.
+		while (temp / 25 != 0)
+		{
+			temp /= 25;
+			ans += temp;
+		}
+
+		while (copyTemp / 25 != 0)
+		{
+			copyTemp /= 25;
+			ans += copyTemp;
+		}
+
+		cout << ans << '\n';
+	}
+}
