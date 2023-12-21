090c3007341b4de920fe9a96bcf4c5ccef4ebdf0
KimChowon <chowon986@gmail.com>
2023-12-21 14:27:58 +0900

    김초원 : 균형잡힌 세상

---
+++ 백준 알고리즘/균형잡힌 세상/균형잡힌 세상.cpp
@@ -0,0 +1,58 @@
+﻿// 균형잡힌 세상
+
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	string s;
+	while (true)
+	{
+		getline(cin, s);
+		if (s == ".")
+			return 0;
+
+		if (s[0] == ')' || s[0] == ']')
+		{
+			cout << "no" << '\n';
+		}
+		else
+		{
+			bool isYes = true;
+			stack<char> st;
+
+			for (int i = 0; i < s.size(); i++)
+			{
+				if (s[i] == '(' || s[i] == '[')
+				{
+					st.push(s[i]);
+				}
+				else if (s[i] == ')')
+				{
+					if (st.empty())
+						isYes = false;
+					else if (st.top() == '(')
+						st.pop();
+					else
+						isYes = false;
+				}
+				else if (s[i] == ']')
+				{
+					if (st.empty())
+						isYes = false;
+					else if (st.top() == '[')
+						st.pop();
+					else
+						isYes = false;
+				}
+			}
+
+			if (st.empty() && isYes)
+				cout << "yes" << endl;
+			else
+				cout << "no" << endl;
+		}
+	}
+}
