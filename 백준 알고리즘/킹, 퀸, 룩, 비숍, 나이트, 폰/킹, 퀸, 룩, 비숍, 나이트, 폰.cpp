cf65b6f9e027abf54128006e605c824180b2de2f
KimChowon <chowon986@gmail.com>
2024-01-02 22:08:21 +0900

    김초원 : 킹, 퀸, 룩, 비숍, 나이트, 폰

---
+++ 백준 알고리즘/킹, 퀸, 룩, 비숍, 나이트, 폰/킹, 퀸, 룩, 비숍, 나이트, 폰.cpp
@@ -0,0 +1,18 @@
+﻿// 킹, 퀸, 룩, 비숍, 나이트, 폰
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int k, q, l, b, n, p;
+	int ok = 1;
+	int oq = 1;
+	int ol = 2;
+	int ob = 2;
+	int on = 2;
+	int op = 8;
+	cin >> k >> q >> l >> b >> n >> p;
+
+	cout << ok - k << ' ' << oq - q << ' ' << ol - l << ' ' << ob - b << ' ' << on - n << ' ' << op - p;
+}
