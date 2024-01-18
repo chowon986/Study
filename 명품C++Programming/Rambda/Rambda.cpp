ddf8c8c25be69f311d3a3ac158a930e1b212039a
KimChowon <chowon986@gmail.com>
2024-01-18 12:43:02 +0900

    김초원 : Rambda

---
+++ 명품C++Programming/Rambda/Rambda.cpp
@@ -0,0 +1,60 @@
+﻿// Rambda
+
+#include <iostream>
+#include <functional>
+#include <vector>
+using namespace std;
+
+int CalLam(int _numA, int _numB, /*int(*Func)(int, int)*/ function<int(int, int)> f)
+{
+	return f(_numA, _numB);
+}
+
+int main()
+{
+	// 람다 식
+	// 무명 함수
+	/* 일반함수 vs 람다
+	   함수 실행 위치 밖의 변수에 접근할 수 있는가?
+	   일반 함수는 불가하지만 람다는 가능
+
+	   함수 밖에 있는 데이터를 어떻게 접근하는가?
+	   (1) 매개 변수
+	   (2) 전역 변수
+	*/
+
+	int cnt = 0;
+
+	//auto lamFunc = [&]()
+	//{
+	//	std::cout << "람다";
+	//	cnt++;
+	//};
+
+	auto TestLam = [&](int a, int b)
+	{
+		cnt++;
+		cout << "cnt :" << cnt << endl;
+		return a + b;
+	};
+	
+	CalLam(10, 10, TestLam);
+
+	cout << cnt << endl;
+
+	//lamFunc();
+
+	[](int a)
+	{
+		cout << "매개변수 : " << a << endl;
+		return a;
+	}(10);
+	
+	[](int a /*= 10*/)
+	{
+		cout << "매개변수 : " << a << endl;
+	}(3);
+
+	cout << CalLam(3, 5, [](int a, int b) {return a + b; }) << endl;
+
+}
---
+++ 명품C++Programming/Thread/Thread.cpp
@@ -0,0 +1,79 @@
+﻿// Thread.cpp
+
+#include <iostream>
+#include <thread>
+#include <atomic>
+using namespace std;
+
+// 요즘에는 os에 상관없이 멀티 스레드를 사용할 수 있게 변하고 있음
+// 아직 소켓은 여기에 포함되어 있지 않음 ㅜㅜ
+
+void Hellofunc()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "하세요" << endl;
+		this_thread::sleep_for(std::chrono::milliseconds(500));
+	}
+}
+
+void Byefunc()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "잘가" << endl;
+	}
+}
+
+//int main()
+//{
+//	thread t(Hellofunc);
+//	thread t2([]()
+//		{
+//			for (int i = 0; i < 10; i++)
+//			{
+//				cout << "람다 잘가" << endl;
+//				this_thread::sleep_for(std::chrono::milliseconds(500));
+//			}
+//		});
+//	// thread 변수명(함수명);
+//
+//	for (int i = 0; i < 10; i++)
+//	{
+//		cout << "안녕하세요" << endl;
+//		this_thread::sleep_for(std::chrono::milliseconds(500));
+//	}
+//
+//	t.join();
+//	t2.join();
+//
+//	return 0;
+//}
+
+int main()
+{
+	// atomic
+	// 전부 처리하거나 아무것도 하지 못하게 함
+	atomic<int> num = 0;
+	thread tA([&]()
+		{
+			for (int i = 0; i < 100000; i++)
+			{
+				num++;
+			}
+		});
+
+	thread tB([&]()
+		{
+			for (int i = 0; i < 100000; i++)
+			{
+				num--;
+			}
+		});
+
+
+	tA.join();
+	tB.join();
+
+	cout << num << endl;
+}
