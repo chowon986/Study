7e78a7da16794a39cb2f2620f60ee620ef86dd9e
김초원 <97011721+chowon986@users.noreply.github.com>
2024-01-21 00:31:12 +0900

    김초원 : 이상한 곱셈

---
+++ 백준 알고리즘/이상한 곱셈/이상한 곱셈.cpp
@@ -0,0 +1,41 @@
+﻿// 이상한 곱셈
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string A, B;
+	cin >> A >> B;
+
+	if (A == "0" || B == "0")
+	{
+		cout << 0;
+		return 0;
+	}
+
+	vector<int> vA, vB;
+
+	for (int i = 0; i < A.size(); i++)
+	{
+		vA.push_back(A[i] - '0');
+	}
+
+	for (int i = 0; i < B.size(); i++)
+	{
+		vB.push_back(B[i] - '0');
+	}
+
+	long long sum = 0;
+	for (int i = 0; i < vA.size(); i++)
+	{
+		for (int j = 0; j < vB.size(); j++)
+		{
+			sum += vA[i] * vB[j];
+		}
+	}
+
+	cout << sum;
+}
