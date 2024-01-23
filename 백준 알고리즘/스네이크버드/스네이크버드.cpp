0d1044e221e22d4996736c3e7319f3fd76e2b0bd
KimChowon <chowon986@gmail.com>
2024-01-23 17:37:23 +0900

    김초원 : 스네이크버드

---
+++ 백준 알고리즘/스네이크버드/스네이크버드.cpp
@@ -0,0 +1,31 @@
+﻿// 스네이크버드
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n, l;
+	cin >> n >> l;
+
+	vector<int> fruits;
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		fruits.push_back(temp);
+	}
+
+	sort(fruits.begin(), fruits.end());
+
+	for (int i = 0; i < n; i++)
+	{
+		if (l >= fruits[i])
+			++l;
+		else
+			break;
+	}
+	cout << l;
+}
