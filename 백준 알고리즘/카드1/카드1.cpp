11ddb6d859cc79c4d79ca1b5abeb52de1012e113
ChowonKim <97011721+chowon986@users.noreply.github.com>
2024-01-24 19:20:38 +0900

    김초원 : 카드 1

---
+++ 백준 알고리즘/카드1/카드1.cpp
@@ -0,0 +1,31 @@
+﻿// 카드1
+
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	deque<int> dq;
+
+	for (int i = 1; i <= n; i++)
+	{
+		dq.push_back(i);
+	}
+
+	while (!dq.empty())
+	{
+		cout << dq.front() << ' ';
+		dq.pop_front();
+
+		if (dq.empty())
+			break;
+
+		int temp = dq.front();
+		dq.push_back(temp);
+		dq.pop_front();
+	}
+}
