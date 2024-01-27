0cec7442e00dab506bf25bee1c0b9a2b3125b59f
KimChowon <chowon986@gmail.com>
2024-01-27 15:35:24 +0900

    김초원 : 새로운 문자열

---
+++ 백준 알고리즘/새로운 문자열/새로운 문자열.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <deque>
+
+using namespace std;
+
+int main()
+{
+	int cnt;
+	cin >> cnt;
+
+	deque<char> dq;
+	string A, B;
+	cin >> A;
+
+	for (int i = 0; i < cnt; i++)
+		dq.push_back(A[i]);
+
+	string S;
+	cin >> S;
+
+	for (int i = 0; i < cnt; i++)
+	{
+		if (S[i] == 'L')
+		{
+			B += dq.front();
+			dq.pop_front();
+		}
+		else if (S[i] == 'R')
+		{
+			B += dq.back();
+			dq.pop_back();
+		}
+	}
+
+	cout << B;
+}
