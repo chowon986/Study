6aedd24a40e4074dadf9bc3f80c172e904084035
KimChowon <chowon986@gmail.com>
2024-01-29 18:46:36 +0900

    김초원 : 배열 합치기

---
+++ 백준 알고리즘/엄청난 부자2/배열 합치기.cpp
@@ -0,0 +1,30 @@
+﻿// 엄청난 부자2
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	vector<int> v;
+
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 0; i < n + m; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	sort(v.begin(), v.end());
+
+	for (int i : v)
+		cout << i << ' ';
+}
